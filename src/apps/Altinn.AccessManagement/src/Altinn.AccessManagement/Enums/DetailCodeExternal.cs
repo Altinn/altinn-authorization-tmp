a5910594f7fe26c48f5eeff392eb8afe19f56adb
Andreas Isnes <andreas.isnes@gmail.com>
2024-11-29 09:31:26 +0100

    add access mgmt

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement/Enums/DetailCodeExternal.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.AccessManagement.Enums
+{
+    /// <summary>
+    /// Fixed values for DetailCodes
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum DetailCodeExternal
+    {
+        /// <summary>
+        /// Unknown reason should not be used
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Has access by a delegated role in ER or Altinn 
+        /// </summary>
+        RoleAccess = 1,
+
+        /// <summary>
+        /// Has access by direct delegation
+        /// </summary>
+        DelegationAccess = 2,
+
+        /// <summary>
+        /// The service requires explicit access in SRR and the reportee has this
+        /// </summary>
+        SrrRightAccess = 3,
+
+        /// <summary>
+        /// Has not access by a delegation of role in ER or Altinn
+        /// </summary>
+        MissingRoleAccess = 4,
+
+        /// <summary>
+        /// Has not access by direct delegation
+        /// </summary>
+        MissingDelegationAccess = 5,
+
+        /// <summary>
+        /// The service requires explicit access in SRR and the reportee is missing this
+        /// </summary>
+        MissingSrrRightAccess = 6,
+
+        /// <summary>
+        /// The service requires explicit authentication level and the reportee is missing this
+        /// </summary>
+        InsufficientAuthenticationLevel = 7,
+
+        /// <summary>
+        /// The receiver already has the right
+        /// </summary>
+        AlreadyDelegated = 8,
+
+        /// <summary>
+        ///  The receiver has the right based on Access List delegation
+        /// </summary>
+        AccessListValidationPass = 9,
+
+        /// <summary>
+        ///  The receiver does not have the right based on Access List delegation
+        /// </summary>
+        AccessListValidationFail = 10,
+    }
+}
---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement/Enums/InstanceDelegationModeExternal.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using NpgsqlTypes;
+
+namespace Altinn.AccessManagement.Enums;
+
+/// <summary>
+/// Enum defining the delegation mode for instance delegation Normal or PArallelSigning
+/// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum InstanceDelegationModeExternal
+{
+    /// <summary>
+    /// Identifies a unit could also be identified with a Organization number
+    /// </summary>
+    [EnumMember(Value = "normal")]
+    [PgName("normal")]
+    Normal,
+}
