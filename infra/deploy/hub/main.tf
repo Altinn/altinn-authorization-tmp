fadbefaebbfa30c200b45d487ed277ae3f9cbd33
Andreas Isnes Nilsen <andreas.isnes@gmail.com>
2025-01-29 14:59:35 +0100

    Chores/add app conf readers (#288)

--- infra/deploy/hub/main.tf
+++ infra/deploy/hub/main.tf
@@ -243,8 +243,6 @@ resource "azurerm_management_lock" "delete" {
   for_each = { for lock in [
     azurerm_public_ip_prefix.ipv4,
     azurerm_public_ip_prefix.ipv6,
-    azurerm_app_configuration.app_configuration,
-    azurerm_key_vault.key_vault,
     azurerm_virtual_network_gateway.vpn,
     azurerm_storage_account.storage
   ] : lock.name => lock.id }
