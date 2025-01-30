bea4a8319bc882f8366e11367f7ee2da025c59ec
Andreas Isnes <andreas.isnes@gmail.com>
2025-01-30 15:58:37 +0100

    remove endpoint for service bus

--- infra/deploy/spoke/service_bus.tf
+++ infra/deploy/spoke/service_bus.tf
@@ -47,24 +47,24 @@ resource "azurerm_servicebus_namespace" "service_bus" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_endpoint
-resource "azurerm_private_endpoint" "service_bus_private_endpoint" {
-  name                          = "pe${azurerm_servicebus_namespace.service_bus.name}"
-  resource_group_name           = azurerm_resource_group.spoke.name
-  location                      = azurerm_resource_group.spoke.location
-  subnet_id                     = azurerm_subnet.dual_stack["ServiceBus"].id
-  custom_network_interface_name = "nicsb${azurerm_servicebus_namespace.service_bus.name}"
+# resource "azurerm_private_endpoint" "service_bus_private_endpoint" {
+#   name                          = "pe${azurerm_servicebus_namespace.service_bus.name}"
+#   resource_group_name           = azurerm_resource_group.spoke.name
+#   location                      = azurerm_resource_group.spoke.location
+#   subnet_id                     = azurerm_subnet.dual_stack["ServiceBus"].id
+#   custom_network_interface_name = "nicsb${azurerm_servicebus_namespace.service_bus.name}"
 
-  count = local.service_bus_enable_private_endpoint ? 1 : 0
+#   count = local.service_bus_enable_private_endpoint ? 1 : 0
 
-  private_service_connection {
-    name                           = azurerm_servicebus_namespace.service_bus.name
-    private_connection_resource_id = azurerm_servicebus_namespace.service_bus.id
-    is_manual_connection           = false
-    subresource_names              = ["namespace"]
-  }
+#   private_service_connection {
+#     name                           = azurerm_servicebus_namespace.service_bus.name
+#     private_connection_resource_id = azurerm_servicebus_namespace.service_bus.id
+#     is_manual_connection           = false
+#     subresource_names              = ["namespace"]
+#   }
 
-  private_dns_zone_group {
-    name                 = azurerm_servicebus_namespace.service_bus.name
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.hub_service_bus.id]
-  }
-}
+#   private_dns_zone_group {
+#     name                 = azurerm_servicebus_namespace.service_bus.name
+#     private_dns_zone_ids = [data.azurerm_private_dns_zone.hub_service_bus.id]
+#   }
+# }
