c0aefe30d12f2750d917ad9e91a9ea6508a8357b
Marius Thuen <thuen@spirh.no>
2025-02-04 02:14:12 +0100

    Delete DelegationResourceDataService.cs

--- src/apps/Altinn.AccessMgmt.AccessPackages/src/Altinn.AccessMgmt.AccessPackages.Repo/Data/Services/DelegationResourceDataService.cs
+++
@@ -1,6 +0,0 @@
-﻿using Altinn.AccessMgmt.AccessPackages.Repo.Data.Contracts;
-using Altinn.AccessMgmt.DbAccess.Data.Contracts;
-using Altinn.AccessMgmt.DbAccess.Data.Services;
-using Altinn.AccessMgmt.Models;
-
-namespace Altinn.AccessMgmt.AccessPackages.Repo.Data.Services;
