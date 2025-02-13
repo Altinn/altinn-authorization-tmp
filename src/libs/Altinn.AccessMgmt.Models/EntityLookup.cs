c7df16e740af9f38e4272b87e0b821f0aa1ebeb3
Marius Thuen <thuen@spirh.no>
2025-02-13 19:31:31 +0100

    Model - Init

---
+++ src/libs/Altinn.AccessMgmt.Models/EntityLookup.cs
@@ -0,0 +1,38 @@
+﻿namespace Altinn.AccessMgmt.Models;
+
+/// <summary>
+/// Lookup for entity
+/// </summary>
+public class EntityLookup
+{
+    /// <summary>
+    /// Identity
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Entity
+    /// </summary>
+    public Guid EntityId { get; set; }
+
+    /// <summary>
+    /// Key (e.g Party,SSN,OrgNo)
+    /// </summary>
+    public string Key { get; set; }
+
+    /// <summary>
+    /// Value
+    /// </summary>
+    public string Value { get; set; }
+}
+
+/// <summary>
+/// Extended Entity Lookup
+/// </summary>
+public class ExtEntityLookup : EntityLookup
+{
+    /// <summary>
+    /// Entity
+    /// </summary>
+    public Entity Entity { get; set; }
+}
