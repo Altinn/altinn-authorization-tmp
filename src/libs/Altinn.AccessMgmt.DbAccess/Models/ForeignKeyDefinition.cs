55bff962aa893f29613d16919811cea63dd25565
Marius Thuen <thuen@spirh.no>
2025-02-13 19:15:24 +0100

    DbAccess - Init

---
+++ src/libs/Altinn.AccessMgmt.DbAccess/Models/ForeignKeyDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Altinn.AccessMgmt.DbAccess.Helpers;
+
+namespace Altinn.AccessMgmt.DbAccess.Models;
+
+public class ForeignKeyDefinition
+{
+    public string Name { get; set; }
+
+    public Type Base { get; set; }
+    public Type Ref { get; set; }
+    public string BaseProperty { get; set; } //UserId
+    public string RefProperty { get; set; } //Id
+    public string ExtendedProperty { get; set; } //User
+
+    public List<GenericFilter> Filters { get; set; } // Check that Value Property exists ...
+
+    public bool IsList { get; set; }
+    public bool IsOptional { get; set; }
+    public bool UseCascadeDelete { get; set; }
+}
