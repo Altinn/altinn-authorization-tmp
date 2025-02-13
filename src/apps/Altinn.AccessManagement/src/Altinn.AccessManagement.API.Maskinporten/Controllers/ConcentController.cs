3b7e644410c59ec404f33f627877f2538d168df0
Rune T. Larsen <rune@hjemmekino.no>
2025-02-13 18:57:00 +0100

    Fixed basic test

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.API.Maskinporten/Controllers/ConcentController.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.API.Maskinporten/Controllers/ConcentController.cs
@@ -7,21 +7,22 @@ namespace Altinn.AccessManagement.Api.Maskinporten.Controllers
     /// <summary>
     /// Comcent controller for Maskinporten
     /// </summary>
-    [Route("/accessmanagment/api/maskinporten")]
+    [Route("accessmanagment/api/maskinporten/")]
     [ApiController]
     public class ConcentController : ControllerBase
     {
         /// <summary>
         /// 
         /// </summary>
-        [HttpGet("/concent/lookup")]
+        [HttpGet]
+        [Route("concent/lookup")]
         public ActionResult<ConsentInfoMaskinporten> GetConcent(Guid id, string from, string to)
         {
             ConsentInfoMaskinporten consentInfoMaskinporten = new ConsentInfoMaskinporten()
             {
                 Id = id,
-                To = ConsentPartyUrn.PersonId.Create.Create(PersonIdentifier.Parse("01038712345")),
-                From = ConsentPartyUrn.OrganizationId.Create(Core.Models.Register.OrganizationNumber.Parse("123456789")),
+                To = ConsentPartyUrn.PersonId.Create(PersonIdentifier.Parse("01014922047")),
+                From = ConsentPartyUrn.OrganizationId.Create(Core.Models.Register.OrganizationNumber.Parse("910418866")),
                 ConcentRights = new List<ConsentRightExternal>() 
             };
 
---
+++ src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/MaskinPorten/ConcentControllerTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Altinn.AccessManagement.Api.Maskinporten.Controllers;
+using Altinn.AccessManagement.Controllers;
+using Altinn.AccessManagement.Tests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccessMgmt.Tests.Controllers.MaskinPorten
+{
+
+    /// <summary>
+    /// Tests for maskinporten controller for consent
+    /// </summary>
+    public class ConcentControllerTest : IClassFixture<CustomWebApplicationFactory<ConcentController>>
+    {
+        private readonly CustomWebApplicationFactory<ConcentController> _factory;
+
+        /// <summary>
+        /// Concent controller test
+        /// </summary>
+        public ConcentControllerTest(CustomWebApplicationFactory<ConcentController> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetConcent()
+        {
+            HttpClient client = GetTestClient();
+            string url = $"/accessmanagment/api/maskinporten/concent/lookup/?id={Guid.NewGuid()}&from=01017512345&to=12312432545";
+            HttpResponseMessage response = await client.GetAsync(url);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Assert.NotNull(responseContent);
+            Assert.Contains("concent", responseContent);
+        }
+
+
+        private HttpClient GetTestClient()
+        {
+            HttpClient client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                });
+            }).CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
