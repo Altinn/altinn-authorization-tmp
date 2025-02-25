99f5bebc57ff18c7282b3387daf4f3a97a180ae3
Andreas Isnes <andreas.isnes@gmail.com>
2025-02-25 14:26:35 +0100

    remoce access pkgs from libs

--- src/libs/Altinn.AccessMgmt.Repo/Repositories/Contracts/IAssignmentResourceRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using Altinn.AccessMgmt.Core.Models;
-using Altinn.AccessMgmt.Persistence.Core.Contracts;
-
-namespace Altinn.AccessMgmt.Persistence.Repositories.Contracts;
-
-/// <inheritdoc/>
-public interface IAssignmentResourceRepository : IDbCrossRepository<AssignmentResource, ExtAssignmentResource, Assignment, Resource> { }
--- src/libs/Altinn.AccessMgmt.Repo/Repositories/Contracts/IEntityLookupRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using Altinn.AccessMgmt.Core.Models;
-using Altinn.AccessMgmt.Persistence.Core.Contracts;
-
-namespace Altinn.AccessMgmt.Persistence.Repositories.Contracts;
-
-/// <inheritdoc/>
-public interface IEntityLookupRepository : IDbExtendedRepository<EntityLookup, ExtEntityLookup> { }
