b1b8e1b1054865938e09a5e938bb8186b8cbdd4f
Marius Thuen <thuen@spirh.no>
2025-02-26 08:48:52 +0100

    AutoApi

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.AutoApi/Program.cs
@@ -0,0 +1,33 @@
+using Altinn.AccessMgmt.Persistence.Core.Models;
+using Altinn.AccessMgmt.Repo.Extensions;
+using Npgsql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// move to : C:\r\altinn-authorization\src\apps\Altinn.AccessManagement\src
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(NpgsqlDataSource.Create("Database=newtests;Host=localhost;Username=wigg;Password=jw8s0F4;Include Error Detail=true"));
+
+var config = builder.Configuration.Get<DbAccessConfig>();
+//// builder.ConfigureDb();
+
+builder.AddDb();
+
+var app = builder.Build();
+
+await app.UseDb();
+
+app.MapAllDefinitionEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
