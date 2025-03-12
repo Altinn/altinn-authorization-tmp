596f8e67dcaf1e94b82216681a3807093f272a9b
Marius Thuen <thuen@spirh.no>
2025-03-12 12:02:52 +0100

    Added Role Lookup to hold more keys for roles

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Models/RoleLookup.cs
@@ -0,0 +1,38 @@
+﻿namespace Altinn.AccessMgmt.Core.Models;
+
+/// <summary>
+/// Lookup values for role
+/// </summary>
+public class RoleLookup
+{
+    /// <summary>
+    /// Identity
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Role identifier
+    /// </summary>
+    public Guid RoleId { get; set; }
+
+    /// <summary>
+    /// Key (e.g Party,SSN,OrgNo)
+    /// </summary>
+    public string Key { get; set; }
+
+    /// <summary>
+    /// Value
+    /// </summary>
+    public string Value { get; set; }
+}
+
+/// <summary>
+/// Extended role lookup
+/// </summary>
+public class ExtRoleLookup : RoleLookup
+{
+    /// <summary>
+    /// Role
+    /// </summary>
+    public Role Role { get; set; }
+}
