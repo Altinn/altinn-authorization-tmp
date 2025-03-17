e9219c74bc59caab70f702fb50deb30ac67b6ecd
Rune T. Larsen <rune@hjemmekino.no>
2025-03-17 14:49:34 +0100

    explicit

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Persistence/Migration/v0.20-accessmanagement/03-setup-tables-consent-consentrequest.sql
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Persistence/Migration/v0.20-accessmanagement/03-setup-tables-consent-consentrequest.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS consent.consentrequest
     requestMessage hstore,
     isDeleted bool default False,
     created timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    status consent.status_type NOT NULL DEFAULT 'created',
+    status consent.status_type NOT NULL DEFAULT 'created'::consent.status_type,
     validto  timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     consented timestamp with time zone NULL,
     revoked timestamp with time zone NULL
