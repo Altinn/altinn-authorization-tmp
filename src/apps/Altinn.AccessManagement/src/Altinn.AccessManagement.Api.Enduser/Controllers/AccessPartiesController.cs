c64d1de56bdedc4151dd3d70ba39a10e50601bcd
Andreas Isnes <andreas.isnes@gmail.com>
2025-04-07 16:27:50 +0200

    remove xml comment

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enduser/Controllers/AccessPartiesController.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enduser/Controllers/AccessPartiesController.cs
@@ -41,7 +41,6 @@ public class AccessPartiesController(IAssignmentService assignmentService, IMapp
     /// <param name="party">The GUID identifying the party the authenticated user is acting on behalf of.</param>
     /// <param name="to">The GUID identifying the target party to which the assignment should be created.</param>
     /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
-    /// <returns>
     [HttpPost]
     [Authorize(Policy = AuthzConstants.POLICY_ACCESS_MANAGEMENT_ENDUSER_WRITE)]
     [ServiceFilter(typeof(AuthorizePartyUuidClaimFilter))]
