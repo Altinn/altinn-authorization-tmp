c6aa0893f76ea1267785c247873df9ca10640c85
Andreas Isnes Nilsen <andreas.isnes@gmail.com>
2025-04-10 15:55:07 +0200

    issue(564): Enduser delete/remove rightholder for party (#565)
    * add delete for rightholders
    
    * remove xml comment
    
    * fix service call
    
    * fix validation input typesx
    
    * fix plural copde
    
    * fix sq comments
    
    * format code in action
    
    * update from
    
    * add cascade delete

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enduser/Controllers/AccessPartiesController.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enduser/Controllers/AccessPartiesController.cs
@@ -2,6 +2,7 @@ using System.Net.Mime;
 using Altinn.AccessManagement.Api.Enduser.Mappers;
 using Altinn.AccessManagement.Api.Enduser.Models;
 using Altinn.AccessManagement.Core.Constants;
+using Altinn.AccessManagement.Core.Extensions;
 using Altinn.AccessManagement.Core.Filters;
 using Altinn.AccessMgmt.Core.Models;
 using Altinn.AccessMgmt.Persistence.Core.Models;
@@ -20,8 +21,11 @@ namespace Altinn.AccessManagement.Api.Enduser.Controllers;
 [ApiController]
 [Route("accessmanagement/api/v1/enduser/access/parties")]
 [FeatureGate(AccessManagementEnduserFeatureFlags.ControllerAccessParties)]
-public class AccessPartiesController(IAssignmentService assignmentService, IMapper<AssignmentExternal, Assignment> mapper) : ControllerBase
+[Authorize(Policy = AuthzConstants.SCOPE_PORTAL_ENDUSER)]
+public class AccessPartiesController(IHttpContextAccessor accessor, IAssignmentService assignmentService, IMapper<AssignmentExternal, Assignment> mapper) : ControllerBase
 {
+    private IHttpContextAccessor Accessor { get; } = accessor;
+
     private IAssignmentService AssignmentService { get; } = assignmentService;
 
     private IMapper<AssignmentExternal, Assignment> Mapper { get; } = mapper;
@@ -41,29 +45,24 @@ public class AccessPartiesController(IAssignmentService assignmentService, IMapp
     /// </summary>
     /// <param name="party">The GUID identifying the party the authenticated user is acting on behalf of.</param>
     /// <param name="to">The GUID identifying the target party to which the assignment should be created.</param>
-    /// <returns></returns>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
     [HttpPost]
     [Authorize(Policy = AuthzConstants.POLICY_ACCESS_MANAGEMENT_ENDUSER_WRITE)]
-    [Authorize(Policy = AuthzConstants.SCOPE_PORTAL_ENDUSER)]
     [ServiceFilter(typeof(AuthorizePartyUuidClaimFilter))]
     [ProducesResponseType<AssignmentExternal>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)]
     [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.Json)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ProblemDetails))]
-    public async Task<IActionResult> PostAccessParty([FromQuery] Guid party, [FromQuery] Guid to)
+    public async Task<IActionResult> PostAccessParty([FromQuery] Guid party, [FromQuery] Guid to, CancellationToken cancellationToken = default)
     {
-        // When history is enabled pass partyUuid downstream to GetOrCreateAssignment
-        //// var partyUuid = Accessor.GetPartyUuid();
-
-        // TODO: Andreas - Verify
-        var options = new ChangeRequestOptions()
+        var partyUuid = Accessor.GetPartyUuid();
+        var audit = new ChangeRequestOptions()
         {
-            ChangedBy = party,
+            ChangedBy = partyUuid,
             ChangedBySystem = AuditDefaults.EnduserApi
         };
 
-        var assignment = await AssignmentService.GetOrCreateAssignment2(party, to, "rightholders", options);
+        var assignment = await AssignmentService.GetOrCreateAssignment(party, to, "rightholder", audit, cancellationToken);
 
         if (assignment.IsProblem)
         {
@@ -71,5 +70,38 @@ public class AccessPartiesController(IAssignmentService assignmentService, IMapp
         }
 
         return Ok(Mapper.Map(assignment.Value));
+    }
+
+    /// <summary>
+    /// Creates an assignment between the authenticated user's selected party and the specified target party.
+    /// </summary>
+    /// <param name="party">The GUID identifying the party the authenticated user is acting on behalf of.</param>
+    /// <param name="to">The GUID identifying the target party to which the assignment should be created.</param>
+    /// <param name="cascade">Must be set to true if enduser should delete active foreign connections.</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
+    [HttpDelete]
+    [Authorize(Policy = AuthzConstants.POLICY_ACCESS_MANAGEMENT_ENDUSER_WRITE)]
+    [ServiceFilter(typeof(AuthorizePartyUuidClaimFilter))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> DeleteAccessParty([FromQuery] Guid party, [FromQuery] Guid to, [FromQuery] bool cascade = false, CancellationToken cancellationToken = default)
+    {
+        var partyUuid = Accessor.GetPartyUuid();
+        var audit = new ChangeRequestOptions()
+        {
+            ChangedBy = partyUuid,
+            ChangedBySystem = AuditDefaults.EnduserApi
+        };
+
+        var problem = await AssignmentService.DeleteAssignment(party, to, "rightholder", audit, cascade, cancellationToken);
+
+        if (problem is { })
+        {
+            return problem.ToActionResult();
+        }
+
+        return NoContent();
     }
 }
