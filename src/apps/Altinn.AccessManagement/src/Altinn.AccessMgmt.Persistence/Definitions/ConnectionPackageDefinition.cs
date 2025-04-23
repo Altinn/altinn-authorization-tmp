886b93a74c97477c9a4fdb4d652244a6c6c25e53
Marius Thuen <thuen@spirh.no>
2025-04-23 12:04:54 +0200

    Added EntityVariant Filter to ConnectionPackage view #622

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Persistence/Definitions/ConnectionPackageDefinition.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Persistence/Definitions/ConnectionPackageDefinition.cs
@@ -18,7 +18,7 @@ public class ConnectionPackageDefinition : BaseDbDefinition<ConnectionPackage>, 
     {
         definitionRegistry.Define<ConnectionPackage>(def =>
         {
-            def.SetVersion(2);
+            def.SetVersion(3);
             def.IsView();
 
             def.RegisterProperty(t => t.ConnectionId);
@@ -38,6 +38,14 @@ public class ConnectionPackageDefinition : BaseDbDefinition<ConnectionPackage>, 
             sb.AppendLine($"SELECT a.{nameof(Assignment.Id)} AS {nameof(ConnectionPackage.ConnectionId)}, rp.{nameof(RolePackage.PackageId)} AS {nameof(ConnectionPackage.PackageId)}");
             sb.AppendLine("FROM dbo.rolepackage AS rp");
             sb.AppendLine("INNER JOIN dbo.assignment AS a ON rp.roleid = a.roleid");
+            sb.AppendLine("WHERE rp.entityvariantid IS NULL");
+
+            sb.AppendLine("UNION ALL");
+            sb.AppendLine($"SELECT a.{nameof(Assignment.Id)} AS {nameof(ConnectionPackage.ConnectionId)}, rp.{nameof(RolePackage.PackageId)} AS {nameof(ConnectionPackage.PackageId)}");
+            sb.AppendLine("FROM dbo.rolepackage AS rp");
+            sb.AppendLine("INNER JOIN dbo.assignment AS a ON rp.roleid = a.roleid");
+            sb.AppendLine("INNER JOIN dbo.entity AS fe ON a.fromid = fe.id and fe.variantid = rp.entityvariantid");
+            sb.AppendLine("WHERE rp.entityvariantid IS NOT NULL");
 
             sb.AppendLine("UNION ALL");
             sb.AppendLine($"SELECT dp.{nameof(DelegationPackage.DelegationId)} AS {nameof(ConnectionPackage.ConnectionId)}, dp.{nameof(DelegationPackage.PackageId)} AS {nameof(ConnectionPackage.PackageId)}");
