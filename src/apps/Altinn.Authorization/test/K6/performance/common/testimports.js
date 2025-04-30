019a809d16a8e82c18416050eed4d13ecad667be
Dagfinn Olsen <dagfinno@gmail.com>
2025-04-30 13:56:18 +0200

    removed unused files

--- src/apps/Altinn.Authorization/test/K6/performance/common/testimports.js
+++ src/apps/Altinn.Authorization/test/K6/performance/common/testimports.js
@@ -1,4 +1,2 @@
 export { chai, expect, expectStatusFor } from './k6chai.js';
-export { uuidv4, uuidv7} from './uuid.js';
 export { describe } from './describe.js';
-export { customConsole  } from './console.js';
