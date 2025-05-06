64da1ab1acc442740b02306e4b2b012e48772d8d
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-05-06 10:43:13 +0200

    updated conf

--- src/apps/Altinn.AccessManagement/test/K6-functional-test/config.js
+++ src/apps/Altinn.AccessManagement/test/K6-functional-test/config.js
@@ -1,17 +1,7 @@
-const env = __ENV.ENVIRONMENT || "at22"; // default to "at22" if not set
-
-const baseUrls = {
-  at22: "https://platform.at22.altinn.cloud",
-  tt02: "https://tt02.altinn.no",
-};
-
 export const config = {
-  env: env,
-  baseUrl: baseUrls[env], // Pick based on environment
-  subscriptionKey:
-    env === "tt02"
-      ? __ENV.TT02_REGISTER_SUBSCRIPTION_KEY
-      : __ENV.AT22_REGISTER_SUBSCRIPTION_KEY,
+  env: __ENV.ENVIRONMENT,
+  baseUrl: __ENV.BASE_URL,
+  subscriptionKey: __ENV.REGISTER_SUBSCRIPTION_KEY,
   soapUsername: __ENV.SOAP_ER_USERNAME,
   soapPassword: __ENV.SOAP_ER_PASSWORD,
   tokenUsername: __ENV.TOKEN_GENERATOR_USERNAME,
