5fcef9a971d9f3b9646941d1b8c9774cdec03a74
Rune T. Larsen <rune@hjemmekino.no>
2025-05-08 20:35:29 +0200

    Added more text

--- src/apps/Altinn.Authorization/test/Altinn.Authorization.Tests/ResourceRegistry_DecisionTests.cs
+++ src/apps/Altinn.Authorization/test/Altinn.Authorization.Tests/ResourceRegistry_DecisionTests.cs
@@ -259,9 +259,25 @@ namespace Altinn.Platform.Authorization.IntegrationTests
         }
 
         [Fact]
-        public async Task PDP_Decision_ResourceRegistry_RequestConsent()
+        public async Task PDP_Decision_ResourceRegistry_RequestConsent_ValidAccessList()
         {
             string testCase = "AltinnResourceRegistry_RequestConsent_ValidAccessList";
+            HttpClient client = GetTestClient();
+
+            HttpRequestMessage httpRequestMessage = TestSetupUtil.CreateJsonProfileXacmlRequest(testCase);
+            XacmlJsonResponse expected = TestSetupUtil.ReadExpectedJsonProfileResponse(testCase);
+
+            // Act
+            XacmlJsonResponse contextResponse = await TestSetupUtil.GetXacmlJsonProfileContextResponseAsync(client, httpRequestMessage);
+
+            // Assert
+            AssertionUtil.AssertEqual(expected, contextResponse);
+        }
+
+        [Fact]
+        public async Task PDP_Decision_ResourceRegistry_RequestConsent_InValidAccessList()
+        {
+            string testCase = "AltinnResourceRegistry_RequestConsent_InValidAccessList";
             HttpClient client = GetTestClient();
 
             HttpRequestMessage httpRequestMessage = TestSetupUtil.CreateJsonProfileXacmlRequest(testCase);
