4545aa1ddb252ba33d40ca2095147519f39ca60a
Rune T. Larsen <rune@hjemmekino.no>
2025-05-13 08:28:59 +0200

    Newtonsoft bye bye

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enterprise/Utils/OrgUtil.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enterprise/Utils/OrgUtil.cs
@@ -1,7 +1,7 @@
 ﻿using System.Security.Claims;
+using System.Text.Json;
 using Altinn.Authorization.Core.Models.Consent;
 using Altinn.Authorization.Core.Models.Register;
-using Newtonsoft.Json.Linq;
 
 namespace Altinn.AccessManagement.Api.Enterprise.Utils
 {
@@ -13,7 +13,7 @@ namespace Altinn.AccessManagement.Api.Enterprise.Utils
         /// <summary>
         /// Returns partyUid claim value if present. Null if not
         /// </summary>
-        public static ConsentPartyUrn? GetAuthenticatedParty(ClaimsPrincipal claimsPrincipal)
+       public static ConsentPartyUrn? GetAuthenticatedParty(ClaimsPrincipal claimsPrincipal)
         {
             string? consumerJson = claimsPrincipal.FindFirstValue("consumer");
             if (string.IsNullOrEmpty(consumerJson))
@@ -21,24 +21,30 @@ namespace Altinn.AccessManagement.Api.Enterprise.Utils
                 return null;
             }
 
-            JObject consumer = JObject.Parse(consumerJson);
+            try
+            {
+                using JsonDocument doc = JsonDocument.Parse(consumerJson);
+                JsonElement root = doc.RootElement;
 
-            if (consumer["ID"] == null || consumer["authority"] == null)
-            {
-                return null;
-            }
-            else
-            {
-                string consumerAuthority = consumer["authority"]!.ToString();
+                if (!root.TryGetProperty("ID", out JsonElement idElement) ||
+                    !root.TryGetProperty("authority", out JsonElement authorityElement))
+                {
+                    return null;
+                }
+
+                string consumerAuthority = authorityElement.GetString()!;
                 if (!"iso6523-actorid-upis".Equals(consumerAuthority))
                 {
                     return null;
                 }
 
-                string consumerId = consumer["ID"]!.ToString();
-
-                string organisationNumber = consumerId.Split(":")[1];
+                string consumerId = idElement.GetString()!;
+                string organisationNumber = consumerId.Split(':')[1];
                 return ConsentPartyUrn.OrganizationId.Create(OrganizationNumber.Parse(organisationNumber));
+            }
+            catch (JsonException)
+            {
+                return null;
             }
         }
     }
