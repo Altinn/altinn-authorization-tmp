636b4829e625b18e6adbde3f818d2353e58789ff
Marius Thuen <thuen@spirh.no>
2025-05-26 07:51:16 +0200

    Update IConnectionV2Service.cs

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Integration/Contracts/IConnectionV2Service.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Integration/Contracts/IConnectionV2Service.cs
@@ -1,4 +1,5 @@
-﻿using Altinn.AccessMgmt.Integration.Models;
+﻿using Altinn.AccessMgmt.Core.Models;
+using Altinn.AccessMgmt.Integration.Models;
 
 namespace Altinn.AccessMgmt.Integration.Contracts;
 
@@ -26,10 +27,34 @@ public interface IConnectionV2Service
     Task<List<ConnectionV2Dto>> GetConnectionsTo(Guid? partyId = null, Guid? roleId = null, Guid? packageId = null);
 
     /// <summary>
+    /// Get Connections via party
+    /// </summary>
+    /// <param name="partyId">Filter for party</param>
+    /// <param name="roleId">Filter for role</param>
+    /// <param name="packageId">Filter for package</param>
+    /// <returns></returns>
+    Task<List<ConnectionV2Dto>> GetConnectionsVia(Guid? partyId = null, Guid? roleId = null, Guid? packageId = null);
+
+    /// <summary>
     /// Get parties with access to package on party
     /// </summary>
     /// <param name="from">Party to check</param>
     /// <param name="package">Filter for packag</param>
     /// <returns></returns>
     Task<List<ConnectionPermission>> GetPackagePermissions(Guid from, Guid package);
+
+    /// <summary>
+    /// Get parties with access to package on party
+    /// </summary>
+    Task<List<CompactPackage>> GetPackagesFrom(Guid? partyId = null, Guid? toId = null, Guid? packageId = null);
+
+    /// <summary>
+    /// Get parties with access to package on party
+    /// </summary>
+    Task<List<CompactPackage>> GetPackagesTo(Guid? partyId = null, Guid? fromId = null, Guid? packageId = null);
+
+    /// <summary>
+    /// Get parties with access to package on party
+    /// </summary>
+    Task<List<CompactResource>> GetResources(Guid? fromId = null, Guid? toId = null, Guid? resourceId = null);
 }
