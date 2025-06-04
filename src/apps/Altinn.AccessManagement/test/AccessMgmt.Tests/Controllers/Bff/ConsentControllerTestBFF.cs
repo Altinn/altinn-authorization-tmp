903d7417f51c67752b343dd2ff3f4265263e82f5
Rune T. Larsen <rune@hjemmekino.no>
2025-06-04 14:24:49 +0200

    Correct naming

--- src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/Bff/ConsentControllerTestBFF.cs
+++ src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/Bff/ConsentControllerTestBFF.cs
@@ -22,7 +22,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
-namespace AccessMgmt.Tests.Controllers.Internal
+namespace AccessMgmt.Tests.Controllers.Bff
 {
     public class ConsentControllerTestBFF(WebApplicationFixture fixture) : IClassFixture<WebApplicationFixture>
     {
