ce9fa65d31ca577cd04ab564f0ba73903bc54738
Rune T. Larsen <rune@hjemmekino.no>
2025-06-20 20:00:10 +0200

    Trying to rename test to see if this help

--- src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/MaskinportenSchemaControllerSuperTest.cs
+++ src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/MaskinportenSchemaControllerSuperTest.cs
@@ -35,7 +35,7 @@ namespace Altinn.AccessManagement.Tests.Controllers
     /// Test class for <see cref="MaskinportenSchemaController"></see>
     /// </summary>
     [Collection("MaskinportenSchemaController Tests")]
-    public class MaskinportenSchemaControllerTest : IClassFixture<CustomWebApplicationFactory<MaskinportenSchemaController>>
+    public class MaskinportenSchemaControllerSuperTest : IClassFixture<CustomWebApplicationFactory<MaskinportenSchemaController>>
     {
         private readonly CustomWebApplicationFactory<MaskinportenSchemaController> _factory;
         private HttpClient _client;
@@ -46,7 +46,7 @@ namespace Altinn.AccessManagement.Tests.Controllers
         /// Constructor setting up factory, test client and dependencies
         /// </summary>
         /// <param name="factory">CustomWebApplicationFactory</param>
-        public MaskinportenSchemaControllerTest(CustomWebApplicationFactory<MaskinportenSchemaController> factory)
+        public MaskinportenSchemaControllerSuperTest(CustomWebApplicationFactory<MaskinportenSchemaController> factory)
         {
             _factory = factory;
             _client = GetTestClient();
