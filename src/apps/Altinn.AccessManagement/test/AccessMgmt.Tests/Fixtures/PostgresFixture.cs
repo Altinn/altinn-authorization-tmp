839f1fb7e108e053c08d6522b9b928b8679f95ae
Rune T. Larsen <rune@hjemmekino.no>
2025-06-23 14:44:41 +0200

    Disable pooling

--- src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Fixtures/PostgresFixture.cs
+++ src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Fixtures/PostgresFixture.cs
@@ -218,9 +218,7 @@ public class PostgresDatabase(string dbname, string connectionString) : IOptions
         Username = PostgresServer.DbAdminName,
         Password = PostgresServer.DbPassword,
         IncludeErrorDetail = true,
-        Pooling = true,      // or "false" to disable pooling entirely
-        MaxPoolSize = 5,         // no more than 5 simultaneous connections
-        MinPoolSize = 0,
+        Pooling = false,      // or "false" to disable pooling entirely
         ConnectionIdleLifetime = 30,  // (optional) return idle connections quickly
     };
 
@@ -234,9 +232,7 @@ public class PostgresDatabase(string dbname, string connectionString) : IOptions
         Username = PostgresServer.DbUserName,
         Password = PostgresServer.DbPassword,
         IncludeErrorDetail = true,
-        Pooling = true,      // or "false" to disable pooling entirely
-        MaxPoolSize = 5,         // no more than 5 simultaneous connections
-        MinPoolSize = 0,
+        Pooling = false,      // or "false" to disable pooling entirely
         ConnectionIdleLifetime = 30,  // (optional) return idle connections quickly
     };
 
