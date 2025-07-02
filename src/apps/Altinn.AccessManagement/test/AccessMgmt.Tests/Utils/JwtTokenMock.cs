1e2ebf186f919f3e5deb8d88369e5aa7afabb2da
Andreas Isnes <andreas.isnes@gmail.com>
2025-07-02 14:47:44 +0200

    tidy warnings

--- src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Utils/JwtTokenMock.cs
+++ src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Utils/JwtTokenMock.cs
@@ -37,17 +37,23 @@ namespace Altinn.AccessManagement.Tests.Utils
             return serializedToken;
         }
 
-        private static SigningCredentials GetSigningCredentials(string issuer)
+        private static X509SigningCredentials GetSigningCredentials(string issuer)
         {
             string certPath = "selfSignedTestCertificate.pfx";
+            string certPassword = "qwer1234"; // Use config or env var in real app
+
             if (!issuer.Equals("sbl.authorization") && !issuer.Equals("www.altinn.no") && !issuer.Equals("UnitTest"))
             {
                 certPath = $"{issuer}-org.pfx";
-                X509Certificate2 certIssuer = X509CertificateLoader.LoadCertificateFromFile(certPath);
-                return new X509SigningCredentials(certIssuer, SecurityAlgorithms.RsaSha256);
             }
 
-            X509Certificate2 cert = X509CertificateLoader.LoadPkcs12FromFile(certPath, "qwer1234");
+            var certificates = X509CertificateLoader.LoadPkcs12CollectionFromFile(certPath, certPassword);
+            var cert = certificates.FirstOrDefault(c => c.HasPrivateKey);
+
+            if (cert is null)
+            {
+                throw new Exception($"No valid certificate with private key found in {certPath}");
+            }
 
             return new X509SigningCredentials(cert, SecurityAlgorithms.RsaSha256);
         }
