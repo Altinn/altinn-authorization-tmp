efb051be359d05b2841de00bf0394cdb053460ec
Jon Kjetil Øye <acn-joye@ai-dev.no>
2025-07-10 15:20:00 +0200

    AccMgmt | Make hovedadministrator package not assignable to organizations #827

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enduser/EnduserValidationRules.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.Enduser/EnduserValidationRules.cs
@@ -259,6 +259,31 @@ public static class EnduserValidationRules
         };
 
         /// <summary>
+        /// Checks the list of packages all are assignable to the recipient entity type.
+        /// </summary>
+        /// <param name="packages">list of packages</param>
+        /// <param name="toEntity">entity the assignment is to be made to</param>
+        /// <param name="paramName">name of the query parameter</param>
+        /// <returns></returns>
+        internal static RuleExpression PackageIsAssignableToRecipient(IEnumerable<ExtConnectionPackage> packages, ExtEntity toEntity, string paramName = "packageId") => () =>
+        {
+            ArgumentNullException.ThrowIfNull(packages);
+            ArgumentException.ThrowIfNullOrEmpty(paramName);
+
+            var packagesNotAssignableToOrg = packages
+                .Where(p => p.Package.Urn.Equals("urn:altinn:accesspackage:hovedadministrator"))
+                .Select(p => p.Package.Urn);
+
+            if (toEntity.Type.Id == EntityTypeId.Organization && packagesNotAssignableToOrg.Any())
+            {
+                return (ref ValidationErrorBuilder errors) =>
+                    errors.Add(ValidationErrors.InvalidQueryParameter, $"QUERY/{paramName}", [new("Packages", $"{string.Join(", ", packagesNotAssignableToOrg)} are not assignable to an organization.")]);
+            }
+
+            return null;
+        };
+
+        /// <summary>
         /// Checks is packages can be asigned by checking <see cref="ConnectionPackage.CanAssign"/> is set to to true.
         /// </summary>
         /// <param name="packages">List of packages.</param>
