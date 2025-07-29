bdaf7039e3f35499c88735942b2c6d71b38c291d
Andreas Isnes <andreas.isnes@gmail.com>
2025-07-29 10:17:47 +0200

    add grafana to tf

--- infra/deploy/hub/telemetry.tf
+++ infra/deploy/hub/telemetry.tf
@@ -13,3 +13,29 @@ resource "azurerm_application_insights" "telemetry" {
   application_type    = "web"
   workspace_id        = azurerm_log_analytics_workspace.telemetry.id
 }
+
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                  = "grafana${local.suffix}"
+  location              = azurerm_resource_group.hub.location
+  resource_group_name   = azurerm_resource_group.hub.name
+  sku                   = "Standard"
+  grafana_major_version = 11
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "grafana_admin" {
+  scope                = azurerm_resource_group.hub.id
+  role_definition_name = "Grafana Admin"
+  principal_id         = each.value
+  for_each             = toset(var.developer_prod_principal_ids)
+}
+
+resource "azurerm_role_assignment" "grafana_editors" {
+  scope                = azurerm_resource_group.hub.id
+  role_definition_name = "Grafana Editor"
+  principal_id         = each.value
+  for_each             = toset(var.developer_prod_principal_ids)
+}
+
