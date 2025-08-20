baa63c4dd69bfefc5f327470bb6ae79e96e70a2c
Aleksander Heintz <alxandr@alxandr.me>
2025-08-20 09:17:09 +0200

    infra: new config values for register (#1098)

--- src/apps/Altinn.Register/infra/main.tf
+++ src/apps/Altinn.Register/infra/main.tf
@@ -190,17 +190,21 @@ module "appsettings" {
   labels = {
     "${var.environment}-register" = {
       values = {
-        "Altinn:MassTransit:register:AzureServiceBus:Endpoint" = { value : "sb://sb${local.spoke_suffix}.servicebus.windows.net" }
-        "A2PartyImport:BridgeApiEndpoint"                      = { value : var.sbl_endpoint }
+        "Altinn:MassTransit:register:AzureServiceBus:Endpoint" = { value = "sb://sb${local.spoke_suffix}.servicebus.windows.net" }
+        "A2PartyImport:BridgeApiEndpoint"                      = { value = var.sbl_endpoint }
 
         // features
-        "Altinn:register:PartyImport:A2:Enable"             = { value : var.features.a2_party_import.parties }
-        "Altinn:register:PartyImport:A2:PartyUserId:Enable" = { value : var.features.a2_party_import.user_ids }
+        "Altinn:register:PartyImport:A2:Enable"             = { value = var.features.a2_party_import.parties }
+        "Altinn:register:PartyImport:A2:PartyUserId:Enable" = { value = var.features.a2_party_import.user_ids }
+        "Altinn:register:PartyImport:A2:Profiles:Enable"    = { value = var.features.a2_party_import.profiles }
+
+        // config
+        "Altinn:register:PartyImport:A2:MaxDbSizeInGib" = { value = var.config.a2_party_import.max_db_size_in_gib }
       }
 
       vault_references = {
-        "Altinn:Npgsql:register:ConnectionString"         = { vault_key_reference : data.azurerm_key_vault_secret.postgres_app.versionless_id }
-        "Altinn:Npgsql:register:Migrate:ConnectionString" = { vault_key_reference : data.azurerm_key_vault_secret.postgres_migration.versionless_id }
+        "Altinn:Npgsql:register:ConnectionString"         = { vault_key_reference = data.azurerm_key_vault_secret.postgres_app.versionless_id }
+        "Altinn:Npgsql:register:Migrate:ConnectionString" = { vault_key_reference = data.azurerm_key_vault_secret.postgres_migration.versionless_id }
       }
     }
   }
