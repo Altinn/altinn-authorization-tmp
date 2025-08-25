dd9503db1ddb3739c73ea2441a29461b28476771
Remi Løvoll <remi.lovoll@avanade.com>
2025-08-25 16:11:51 +0200

    Added one test to get better code coverage

--- src/apps/Altinn.Authorization/test/Altinn.Authorization.Tests/AltinnApps_DecisionTests.cs
+++ src/apps/Altinn.Authorization/test/Altinn.Authorization.Tests/AltinnApps_DecisionTests.cs
@@ -301,6 +301,21 @@ namespace Altinn.Platform.Authorization.IntegrationTests
         }
 
         [Fact]
+        public async Task PDP_Decision_AltinnApps0028()
+        {
+            string testCase = "AltinnApps0028";
+            HttpClient client = GetTestClient(null, featureManageMock.Object);
+            HttpRequestMessage httpRequestMessage = TestSetupUtil.CreateJsonProfileXacmlRequest(testCase);
+            XacmlJsonResponse expected = TestSetupUtil.ReadExpectedJsonProfileResponse(testCase);
+
+            // Act
+            XacmlJsonResponse contextResponse = await TestSetupUtil.GetXacmlJsonProfileContextResponseAsync(client, httpRequestMessage);
+
+            // Assert
+            AssertionUtil.AssertEqual(expected, contextResponse);
+        }
+
+        [Fact]
         public async Task PDP_Decision_AltinnApps0001_Delegation()
         {
             string testCase = "AltinnApps0001Delegation";
