43c3f53084db95ac87bfcb5b377820961405644c
Aleksander Heintz <alxandr@alxandr.me>
2025-09-09 18:09:08 +0200

    chore: register import features (#1210)

--- src/apps/Altinn.Register/infra/main.tf
+++ src/apps/Altinn.Register/infra/main.tf
@@ -197,6 +197,7 @@ module "appsettings" {
         "Altinn:register:PartyImport:A2:Enable"             = { value = var.features.a2_party_import.parties }
         "Altinn:register:PartyImport:A2:PartyUserId:Enable" = { value = var.features.a2_party_import.user_ids }
         "Altinn:register:PartyImport:A2:Profiles:Enable"    = { value = var.features.a2_party_import.profiles }
+        "Altinn:register:PartyImport:SystemUsers:Enable"    = { value = var.features.party_import.system_users }
 
         // config
         "Altinn:register:PartyImport:A2:MaxDbSizeInGib" = { value = var.config.a2_party_import.max_db_size_in_gib }
