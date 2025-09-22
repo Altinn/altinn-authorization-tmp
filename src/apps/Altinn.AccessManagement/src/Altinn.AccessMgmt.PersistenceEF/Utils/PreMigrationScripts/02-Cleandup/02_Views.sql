e71360a6f27fb6f2ce194defc472208832078cc1
Marius Thuen <thuen@spirh.no>
2025-09-22 10:42:02 +0200

    Updated Post PreMigScripts

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Utils/PreMigrationScripts/02-Cleandup/02_Views.sql
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Utils/PreMigrationScripts/02-Cleandup/02_Views.sql
@@ -1,9 +1,13 @@
+-- Needed for Custom Db Repo
+
+/*
+DROP view dbo.relation;
 drop view dbo.compactrelation;
 drop view dbo.connectionresource;
-drop view dbo.relation;
 
 drop function dbo.set_audit_metadata_fn();
 
---drop table dbo._dbmigration;
---drop table dbo.statusrecord;
---alter schema dbo_history rename to history;
+drop table dbo._dbmigration;
+drop table dbo.statusrecord;
+alter schema dbo_history rename to history;
+*/
