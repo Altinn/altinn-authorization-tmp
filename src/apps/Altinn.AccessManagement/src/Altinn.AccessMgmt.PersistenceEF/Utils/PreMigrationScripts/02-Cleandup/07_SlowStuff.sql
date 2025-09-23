32b8f23885d283ab9ab320fa1d16d023c8b9bdff
Marius Thuen <thuen@spirh.no>
2025-09-23 13:28:28 +0200

    Update insert trigger function

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Utils/PreMigrationScripts/02-Cleandup/07_SlowStuff.sql
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Utils/PreMigrationScripts/02-Cleandup/07_SlowStuff.sql
@@ -3,19 +3,9 @@ alter table dbo_history.auditentitylookup
         primary key (id, audit_validfrom, audit_validto);
 
 alter table dbo_history.auditentitylookup
-    alter column audit_changedby drop not null;
-
-alter table dbo_history.auditentitylookup
-    alter column audit_changedbysystem drop not null;
-
-alter table dbo_history.auditentitylookup
-    alter column audit_changeoperation drop not null;
-
-alter table dbo_history.auditentitylookup
-    alter column entityid set not null;
-
-alter table dbo_history.auditentitylookup
-    alter column isprotected set not null;
-
-alter table dbo_history.auditentitylookup
+    alter column audit_changedby drop not null,
+    alter column audit_changedbysystem drop not null,
+    alter column audit_changeoperation drop not null,
+    alter column entityid set not null,
+    alter column isprotected set not null,
     alter column isprotected drop default;
