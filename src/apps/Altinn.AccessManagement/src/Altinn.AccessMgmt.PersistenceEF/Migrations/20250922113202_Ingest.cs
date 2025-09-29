09f0bf25bc53e9a02129a850679f2399f3e3fc70
Marius Thuen <thuen@spirh.no>
2025-09-29 13:18:13 +0200

    Update 20250922113202_Ingest.cs

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20250922113202_Ingest.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20250922113202_Ingest.cs
@@ -12,9 +12,9 @@ namespace Altinn.AccessMgmt.PersistenceEF.Migrations
         {
             migrationBuilder.Sql("""
                 CREATE SCHEMA ingest;
-                CREATE ROLE ingest_admins NOLOGIN;
-                GRANT ingest_admins TO platform_authorization_admin, platform_authorization;
-                ALTER SCHEMA ingest OWNER TO ingest_admins;
+                ALTER SCHEMA ingest OWNER TO platform_authorization_admin;
+                GRANT USAGE, CREATE ON SCHEMA ingest TO platform_authorization;
+                GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ingest TO platform_authorization;
                 """);
         }
 
@@ -23,7 +23,6 @@ namespace Altinn.AccessMgmt.PersistenceEF.Migrations
         {
             migrationBuilder.Sql("""
                 DROP SCHEMA ingest CASCADE;
-                DROP ROLE ingest_admins;
                 """);
         }
     }
