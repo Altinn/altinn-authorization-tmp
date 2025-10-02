6b537a80fafdf567e7d7ef4a31859159fdcbd502
Andreas Isnes <andreas.isnes@gmail.com>
2025-10-02 14:29:02 +0200

    update migration stuff

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20251002122143_AddView.Designer.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20251002122143_AddView.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Altinn.AccessMgmt.PersistenceEF.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Altinn.AccessMgmt.PersistenceEF.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251002122143_AddView")]
+    partial class AddView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
