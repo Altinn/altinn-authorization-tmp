47ce83c647f21a417ca83672a67b25156a826dba
Andreas Isnes <andreas.isnes@gmail.com>
2025-10-03 13:08:23 +0200

    include full entity

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,41 @@
+using Altinn.AccessMgmt.PersistenceEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Altinn.AccessMgmt.PersistenceEF.Extensions;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<Connection> IncludeExtendedEntities(this IQueryable<Connection> queryable)
+    {
+        return queryable.Include(c => c.From)
+            .ThenInclude(c => c.Variant)
+            .Include(c => c.From)
+            .ThenInclude(c => c.Type)
+
+            .Include(c => c.From)
+            .ThenInclude(c => c.Parent)
+            .ThenInclude(c => c.Variant)
+            .Include(c => c.From)
+            .ThenInclude(c => c.Parent)
+            .ThenInclude(c => c.Type)
+            .Include(c => c.From)
+            .ThenInclude(c => c.Parent)
+            .ThenInclude(c => c.Variant)
+
+            .Include(c => c.To)
+            .ThenInclude(c => c.Variant)
+            .Include(c => c.To)
+            .ThenInclude(c => c.Type)
+            .Include(c => c.To)
+            .ThenInclude(c => c.Parent)
+
+            .Include(c => c.To)
+            .ThenInclude(c => c.Parent)
+            .ThenInclude(c => c.Variant)
+            .Include(c => c.To)
+            .ThenInclude(c => c.Parent)
+            .ThenInclude(c => c.Type)
+            .Include(c => c.To)
+            .ThenInclude(c => c.Parent);
+    }
+}
