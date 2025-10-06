d5500d6230b5ed590e8978ae5f700cb5bcd9512a
Marius Thuen <thuen@spirh.no>
2025-10-06 12:39:01 +0200

    Update DtoMapper.cs

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Utils/Mappers/DtoMapper.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Utils/Mappers/DtoMapper.cs
@@ -1,8 +1,6 @@
-﻿using Altinn.AccessMgmt.Core.Models;
-using Altinn.AccessMgmt.PersistenceEF.Constants;
+﻿using Altinn.AccessMgmt.PersistenceEF.Constants;
 using Altinn.AccessMgmt.PersistenceEF.Models;
 using Altinn.Authorization.Api.Contracts.AccessManagement;
-using System.Reflection.Metadata.Ecma335;
 
 namespace Altinn.AccessMgmt.Core.Utils;
 
