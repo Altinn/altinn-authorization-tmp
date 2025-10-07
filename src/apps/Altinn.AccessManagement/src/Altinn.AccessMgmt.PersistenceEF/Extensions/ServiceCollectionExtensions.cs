4c96295381e77da83a3d1a9d565dbacbbbb39e5b
Marius Thuen <mariusthuen@gmail.com>
2025-10-07 18:10:07 +0200

    AccessMgmt - Bug Scoped Connections (#1441)
    * Added ConnectionStrings to DbOptions
    
    * Replaced IAltinnDatabase with DbOptions

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/ServiceCollectionExtensions.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/ServiceCollectionExtensions.cs
@@ -6,7 +6,6 @@ using Altinn.AccessMgmt.PersistenceEF.Utils;
 using Altinn.Authorization.Host.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 
@@ -21,24 +20,28 @@ public static class ServiceCollectionExtensions
         services.AddScoped<ReadOnlyInterceptor>();
         services.AddScoped<IAuditAccessor, AuditAccessor>();
         services.AddScoped<ITranslationService, TranslationService>();
-        services.AddScoped<AppDbContextFactory>();
-        services.AddScoped(sp => sp.GetRequiredService<AppDbContextFactory>().CreateDbContext());
         services.AddSingleton<AuditMiddleware>();
         
+        /*
+        // Skal ikke være nødvendig
+        services.AddScoped<AppDbContextFactory>();
+        services.AddScoped(sp => sp.GetRequiredService<AppDbContextFactory>().CreateDbContext());
+        */
+
         if (options.EnableEFPooling)
         {
             return options.Source switch
             {
-                SourceType.App => services.AddPooledDbContextFactory<AppDbContext>(AddAppDbContext),
-                SourceType.Migration => services.AddPooledDbContextFactory<AppDbContext>(AddMigrationDbContext),
+                SourceType.App => services.AddPooledDbContextFactory<AppDbContext>((sp, opt) => AddAppDbContext(sp, opt, options)),
+                SourceType.Migration => services.AddPooledDbContextFactory<AppDbContext>((sp, opt) => AddMigrationDbContext(sp, opt, options)),
                 _ => throw new ArgumentException("Invalid configured source must be either <App, Migration>", nameof(configureOptions)),
             };
         }
 
         return options.Source switch
         {
-            SourceType.App => services.AddDbContextFactory<AppDbContext>(AddAppDbContext),
-            SourceType.Migration => services.AddDbContextFactory<AppDbContext>(AddMigrationDbContext),
+            SourceType.App => services.AddDbContextFactory<AppDbContext>((sp, opt) => AddAppDbContext(sp, opt, options)),
+            SourceType.Migration => services.AddDbContextFactory<AppDbContext>((sp, opt) => AddMigrationDbContext(sp, opt, options)),
             _ => throw new ArgumentException("Invalid configured source must be either <App, Migration>", nameof(configureOptions)),
         };
     }
@@ -48,21 +51,15 @@ public static class ServiceCollectionExtensions
         builder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
     }
 
-    private static void AddMigrationDbContext(IServiceProvider sp, DbContextOptionsBuilder options)
+    private static void AddMigrationDbContext(IServiceProvider sp, DbContextOptionsBuilder options, AccessManagementDatabaseOptions databaseOptions)
     {
-        var db = sp.GetRequiredService<IAltinnDatabase>();
-        var connectionString = db.CreatePgsqlConnection(SourceType.Migration);
-        var configuration = sp.GetRequiredService<IConfiguration>();
         options.UseAsyncSeeding(async (dbcontext, anyChanges, ct) => await StaticDataIngest.IngestAll((AppDbContext)dbcontext, ct));
-        options.UseNpgsql(connectionString, ConfigureNpgsql)
-            .ReplaceService<IMigrationsSqlGenerator, CustomMigrationsSqlGenerator>();
+        options.UseNpgsql(databaseOptions.MigrationConnectionString, ConfigureNpgsql).ReplaceService<IMigrationsSqlGenerator, CustomMigrationsSqlGenerator>();
     }
 
-    private static void AddAppDbContext(IServiceProvider sp, DbContextOptionsBuilder options)
+    private static void AddAppDbContext(IServiceProvider sp, DbContextOptionsBuilder options, AccessManagementDatabaseOptions databaseOptions)
     {
-        var db = sp.GetRequiredService<IAltinnDatabase>();
-        var connectionString = db.CreatePgsqlConnection(SourceType.App);
-        options.UseNpgsql(connectionString, ConfigureNpgsql);
+        options.UseNpgsql(databaseOptions.AppConnectionString, ConfigureNpgsql);
     }
 
     public class AccessManagementDatabaseOptions
@@ -75,5 +72,9 @@ public static class ServiceCollectionExtensions
         public SourceType Source { get; set; } = SourceType.App;
 
         public bool EnableEFPooling { get; set; } = false;
+
+        public string MigrationConnectionString { get; set; } = string.Empty;
+
+        public string AppConnectionString { get; set; } = string.Empty;
     }
 }
