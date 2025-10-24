a41da547e7569d8c1d5db85a9d8aa2984510ca7b
Marius Thuen <thuen@spirh.no>
2025-10-24 09:41:50 +0200

    Added ConnectionQuery

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/QueryableExtensions.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/QueryableExtensions.cs
@@ -11,4 +11,65 @@ public static class QueryableExtensions
     {
         return condition ? source.Where(predicate) : source;
     }
+
+    public static IQueryable<T> WhereEqualsIfSet<T>(
+    this IQueryable<T> query,
+    Guid? id,
+    Expression<Func<T, Guid>> selector)
+    {
+        if (id is null)
+        {
+            return query;
+        }
+
+        var equalExpr = Expression.Equal(selector.Body, Expression.Constant(id.Value));
+        var lambda = Expression.Lambda<Func<T, bool>>(equalExpr, selector.Parameters);
+        return query.Where(lambda);
+    }
+
+    public static IQueryable<T> WhereInIfSet<T>(
+        this IQueryable<T> query,
+        IReadOnlyCollection<Guid>? ids,
+        Expression<Func<T, Guid>> selector)
+    {
+        if (ids is null || ids.Count == 0)
+        {
+            return query;
+        }
+
+        var parameter = selector.Parameters[0];
+        var body = Expression.Call(
+            typeof(Enumerable),
+            nameof(Enumerable.Contains),
+            new[] { typeof(Guid) },
+            Expression.Constant(ids),
+            selector.Body
+        );
+
+        var lambda = Expression.Lambda<Func<T, bool>>(body, parameter);
+        return query.Where(lambda);
+    }
+
+    /// <summary>
+    /// If ids are null or empty do nothing. 
+    /// If ids contains single element use Equals. 
+    /// If ids contains multiple use In.
+    /// </summary>
+    public static IQueryable<T> WhereMatchIfSet<T>(
+     this IQueryable<T> query,
+     IReadOnlyCollection<Guid>? ids,
+     Expression<Func<T, Guid>> selector)
+    {
+        if (ids is null || ids.Count == 0)
+        {
+            return query;
+        }
+
+        if (ids.Count == 1)
+        {
+            return query.WhereEqualsIfSet(ids.First(), selector);
+        }
+
+        return query.WhereInIfSet(ids, selector);
+    }
 }
