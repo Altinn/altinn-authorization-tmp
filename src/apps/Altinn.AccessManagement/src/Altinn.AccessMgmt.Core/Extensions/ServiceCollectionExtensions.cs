2646c1ad81d60fd1d0562e4d4583970471700fe7
Jon Kjetil Øye <acn-joye@ai-dev.no>
2025-10-25 20:20:01 +0200

    feat: AuthorizedParties support for AuthorizedAccessPackages for all party types #1489 - Rewritten AuthorizedParties service using EF services for local db lookups - Added new properties to model: DateOfBirth and ParentId - Feature toggle support for old and new AuthorizedParties service - Added needed EF queries for:   - AssignmentService.GetKeyRoleAssignments   - EntityService.GetByUsername   - EntityService.GetByUsername   - EntityService.GetChildren   - EntityService.GetEntities   - EntityService.GetEntitiesByPartyIds

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Extensions/ServiceCollectionExtensions.cs
@@ -1,18 +1,22 @@
 using Altinn.AccessManagement.Core.Repositories.Interfaces;
+using Altinn.AccessManagement.Core.Services;
 using Altinn.AccessManagement.Core.Services.Contracts;
+using Altinn.AccessManagement.Core.Services.Interfaces;
 using Altinn.AccessMgmt.Core.HostedServices;
 using Altinn.AccessMgmt.Core.HostedServices.Contracts;
 using Altinn.AccessMgmt.Core.HostedServices.Services;
 using Altinn.AccessMgmt.Core.Services;
 using Altinn.AccessMgmt.Core.Services.Contracts;
 using Altinn.AccessMgmt.PersistenceEF.Utils;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AMPartyService = Altinn.AccessMgmt.Core.Services.AMPartyService;
 
 namespace Altinn.AccessMgmt.Core.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddAccessMgmtCore(this IServiceCollection services)
+    public static IServiceCollection AddAccessMgmtCore(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddHostedService<RegisterHostedService>();
         services.AddScoped<RegisterHostedService>();
@@ -26,6 +30,15 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IResourceService, ResourceService>();
         services.AddScoped<IEntityService, EntityService>();
         services.AddScoped<IAuthorizedPartyRepoService, AuthorizedPartyRepoService>();
+
+        if (configuration.GetValue<bool>("FeatureManagement:AccessMgmt.Core.Services.AuthorizedParties.EfEnabled"))
+        {
+            services.AddScoped<IAuthorizedPartiesService, AuthorizedPartiesServiceEf>();
+        }
+        else
+        {
+            services.AddScoped<IAuthorizedPartiesService, AuthorizedPartiesService>();
+        }
 
         services.AddScoped<IAmPartyRepository, AMPartyService>();
         services.AddScoped<IEntityService, EntityService>();
