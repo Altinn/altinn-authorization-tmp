f1ae72fa69c7e8cfb3de270fc7e3e4f0b9e500e2
Løvoll, Remi <acn-rlovo@ai-dev.no>
2025-10-28 20:35:10 +0100

    switch from and two so assignment is created in correct direction

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/HostedServices/Services/AltinnAdminRoleSyncService.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/HostedServices/Services/AltinnAdminRoleSyncService.cs
@@ -114,7 +114,7 @@ namespace Altinn.AccessMgmt.Core.HostedServices.Services
                                 continue;
                             }
 
-                            List<AssignmentPackageDto> adds = await assignmentService.ImportAssignmentPackagesPackages(item.ToUserPartyUuid.Value, item.FromPartyUuid, packageUrns, values, cancellationToken);
+                            List<AssignmentPackageDto> adds = await assignmentService.ImportAssignmentPackagesPackages(item.FromPartyUuid, item.ToUserPartyUuid.Value, packageUrns, values, cancellationToken);
                             if (adds.Count == 0)
                             {
                                 _logger.LogWarning(
