a333bc840a08a045522ebda8e99098127778189e
Aleksander Heintz <alxandr@alxandr.me>
2025-11-05 11:19:36 +0100

    feat: create scope package
    BREAKING-CHANGE: This removes `IScopeAccessRequirement`, 
    `ScopeAccessRequirement`, and `ScopeAccessHandler` from the 
    `Altinn.Common.PEP` package. They have been moved to the new 
    `Altinn.Authorization.Scopes` package and renamed to 
    `IAnyOfScopeAuthorizationRequirement`. The implementation types have 
    been made internal, and the handler is added using the extension method 
    `AddAltinnScopesAuthorizationHandlers`.

---
+++ src/pkgs/Altinn.Authorization.PEP/src/Altinn.Authorization.Scopes/Microsoft.AspNetCore.Authorization/AltinnAuthorizationPolicyBuilderExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Altinn.Authorization.Scopes;
+
+namespace Microsoft.AspNetCore.Authorization;
+
+/// <summary>
+/// Provides extension methods for configuring Altinn-specific authorization policies using the
+/// AuthorizationPolicyBuilder.
+/// </summary>
+public static class AltinnAuthorizationPolicyBuilderExtensions
+{
+#if NET9_0_OR_GREATER
+    /// <summary>
+    /// Adds a requirement to the authorization policy that at least one of the specified scopes must be present in the
+    /// user's claims.
+    /// </summary>
+    /// <param name="builder">The <see cref="AuthorizationPolicyBuilder"/> to which the scope requirement will be added.</param>
+    /// <param name="scopes">An array of scope names. At least one of these scopes must be present in the user's claims to satisfy the
+    /// requirement. Cannot be null or empty.</param>
+    /// <returns>The <see cref="AuthorizationPolicyBuilder"/> instance with the added scope requirement.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="scopes"/> is null or empty.</exception>
+    [System.Runtime.CompilerServices.OverloadResolutionPriority(1)]
+    public static AuthorizationPolicyBuilder RequireAnyScopeOf(
+        this AuthorizationPolicyBuilder builder,
+        params System.Collections.Immutable.ImmutableArray<string> scopes)
+    {
+        if (scopes.IsDefaultOrEmpty)
+        {
+            throw new ArgumentException("Scopes cannot be null or empty.", nameof(scopes));
+        }
+
+        builder.Requirements.Add(new AnyOfScopeAuthorizationRequirement([.. scopes]));
+        return builder;
+    }
+#endif
+
+    /// <summary>
+    /// Adds a requirement to the authorization policy that at least one of the specified scopes must be present in the
+    /// user's claims.
+    /// </summary>
+    /// <param name="builder">The <see cref="AuthorizationPolicyBuilder"/> to which the scope requirement will be added.</param>
+    /// <param name="scopes">An array of scope names. At least one of these scopes must be present in the user's claims to satisfy the
+    /// requirement. Cannot be null or empty.</param>
+    /// <returns>The <see cref="AuthorizationPolicyBuilder"/> instance with the added scope requirement.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="scopes"/> is null or empty.</exception>
+    public static AuthorizationPolicyBuilder RequireAnyScopeOf(
+        this AuthorizationPolicyBuilder builder,
+        params string[] scopes)
+    {
+        if (scopes is null or { Length: 0 })
+        {
+            throw new ArgumentException("Scopes cannot be null or empty.", nameof(scopes));
+        }
+
+        builder.Requirements.Add(new AnyOfScopeAuthorizationRequirement([.. scopes]));
+        return builder;
+    }
+}
---
+++ src/pkgs/Altinn.Authorization.PEP/src/Altinn.Authorization.Scopes/Microsoft.Extensions.DependencyInjection/AltinnAuthorizationScopesServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Altinn.Authorization.Scopes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class AltinnAuthorizationScopesServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers Altinn scopes-based authorization handlers with the specified service collection.
+    /// </summary>
+    /// <param name="services">The service collection to which the authorization handler will be added. Cannot be null.</param>
+    /// <returns><paramref name="services"/>.</returns>
+    public static IServiceCollection AddAltinnScopesAuthorizationHandlers(this IServiceCollection services)
+    {
+        services.AddAnyOfScopeAuthorizationHandler();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers the AnyOfScopeAuthorizationHandler for scope-based authorization in the application's dependency
+    /// injection container.
+    /// </summary>
+    /// <param name="services">The service collection to which the authorization handler will be added. Cannot be null.</param>
+    /// <returns><paramref name="services"/>.</returns>
+    public static IServiceCollection AddAnyOfScopeAuthorizationHandler(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IAuthorizationHandler, AnyOfScopeAuthorizationHandler>();
+
+        return services;
+    }
+}
