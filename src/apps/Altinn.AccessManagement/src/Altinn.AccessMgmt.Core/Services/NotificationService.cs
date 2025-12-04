f2375acd110281ccb49a63f5f2800bef01f065f5
Marius Thuen <thuen@spirh.no>
2025-12-04 18:50:39 +0100

    Create NotificationService.cs

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/NotificationService.cs
@@ -0,0 +1,15 @@
+﻿namespace Altinn.AccessMgmt.Core.Services;
+
+/// <inheritdoc />
+public class NotificationService : INotificationService
+{
+
+}
+
+/// <summary>
+/// Service to collect, group and send notifications
+/// </summary>
+public interface INotificationService
+{
+
+}
