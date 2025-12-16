42a405ac8e2088a1bf95048971d7a695da19cba5
Løvoll, Remi <acn-rlovo@ai-dev.no>
2025-12-16 11:32:01 +0100

    Do not import tilganstyrer as package for klientdelegeringer

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/HostedServices/Services/AltinnAdminRoleSyncService.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/HostedServices/Services/AltinnAdminRoleSyncService.cs
@@ -120,6 +120,16 @@ namespace Altinn.AccessMgmt.Core.HostedServices.Services
                                 continue;
                             }
 
+                            if (item.PerformedByPartyId != null)
+                            {
+                                _logger.LogWarning(
+                                    "The delegation is performed as a client delegation and should not be imported as a package to A3 FromParty {FromParty}, ToParty: {ToParty}, PackageUrns: {PackageUrn}",
+                                    item.FromPartyUuid,
+                                    item.ToUserPartyUuid,
+                                    string.Join(", ", packageUrns));
+                                continue;
+                            }
+
                             List<AssignmentPackageDto> adds = await assignmentService.ImportAssignmentPackages(item.FromPartyUuid, item.ToUserPartyUuid.Value, packageUrns, values, cancellationToken);
                             if (adds.Count == 0)
                             {
