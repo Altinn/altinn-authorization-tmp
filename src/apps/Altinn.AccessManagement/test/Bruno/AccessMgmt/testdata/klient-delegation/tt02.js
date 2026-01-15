c2ddd6dbe842a8dbed84fe7465706ef3294e42be
Sneha Sirure <sneha.sirure@accenture.com>
2026-01-15 10:32:15 +0100

    Added bruno tests for klient-delegation API endpoints

---
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/klient-delegation/tt02.js
@@ -0,0 +1,68 @@
+module.exports = {
+  env: "tt02",
+  resources: {
+    clientDelgResourceId: "devtest_gar_bruno_client_resource",
+  },
+  REGN_Organisasjon: {
+    name: "MOTSTANDSDYKTIG VASSEN TIGER AS",
+    orgno: "314243994",
+    partyId: 51450558,
+    partyUuid: "4a06214d-b261-4695-b33a-0771a995b503",
+    dagligleder: {
+      name: "FORSTÅELSESFULL BYGD",
+      pid: "08885399984",
+      userId: 1260388,
+      partyId: 51395522,
+      partyUuid: "1b490ad0-778b-42d7-a6a6-30b9ea0d726c",
+    },
+    REGN_Org_Clients: {
+      client_A: {
+        name: "AKADEMISK RU TIGER AS",
+        orgno: "213368532",
+        partyId: 51489644,
+        partyUuid: "71b12257-2311-492e-802d-b5a9cff96708",
+        dagligleder: {
+          name: "HALV BEHANDLING",
+          pid: "02826899428",
+          userId: 2469326,
+          partyId: 51180221,
+          partyUuid: "e546521c-2a13-4e0e-a691-c77eb3c81337",
+        },
+      },
+      client_B: {
+        name: "ALFABETISK INITIATIVRIK TIGER AS",
+        orgno: "313360210",
+        partyId: 51840240,
+        partyUuid: "8758a530-988e-4a42-8fea-b75b67f91ed5",
+        dagligleder: {
+          name: "DJERV ABONNEMENT",
+          pid: "20824699322",
+          userId: 2408994,
+          partyId: 51212734,
+          partyUuid: "d791a1a3-94a5-46ba-9bdb-f99e2da5409b",
+        },
+      },
+    },
+    REGN_Agent: {
+      etternavn: "GRANITT",
+      personidentity: "08919574934",
+      userId: 1465828,
+      partyId: 50441038,
+      partyUuid: "01f7a70d-2619-4c50-8ff4-efd7ae6c8960",
+
+    },
+  },
+  REVI_Organisasjon: {
+    name: "OVERFLADISK LANG TIGER AS",
+    orgno: "310267511",
+    partyId: 51449365,
+    partyUuid: "03b14d35-4b8c-44cd-9c71-dc740d8585c2",
+    dagligleder: {
+      name: "ROBUST SERVIETT",
+      pid: "25848395245",
+      userId: 339925,
+      partyId: 50743388,
+      partyUuid: "998b0ee8-57d6-42c3-a36c-a775bbf93210",
+    },
+  },
+};
