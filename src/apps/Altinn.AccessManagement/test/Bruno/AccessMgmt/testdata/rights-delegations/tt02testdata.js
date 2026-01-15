32705b28046c81caf84b573b27bcf9c51142601d
Remi André Løvoll <56019927+lovoll@users.noreply.github.com>
2026-01-15 16:26:51 +0100

    Enforce Personal Token to have PartyUuid claim (#2022)
    * Enforce Personal Token to have PartyUuid claim for Maskinporten and Single App/Resource delegation as it is used to stamp PerformedBy on the actual delegation. If not present return NotAuthorized. Updated some Bruno and Integration tests to reflect this change.
    
    * Fixed Copilot review sugestions:
    - added missing uuid to test
    - removed added empty line
    
    ---------
    
    Co-authored-by: Løvoll, Remi <acn-rlovo@ai-dev.no>

--- src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/rights-delegations/tt02testdata.js
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/rights-delegations/tt02testdata.js
@@ -10,55 +10,64 @@ module.exports =
     "org1": {
         "orgno": "310438707",
         "partyid": 51563583,
+        "partyuuid": "7df033f9-9d72-42c7-a2d8-9533d925ddc5",
         "dagl": {
             "pid":"22855296954",
             "lastname": "LARM",
             "userid": 1278314,
-            "partyid": 50783137
+            "partyid": 50783137,
+            "partyuuid": "a39359de-f1d2-4dc7-b2ba-f9841ea1a799"
         },
         "meda": {
             "pid":"18868496661",
             "lastname": "BALL",
             "userid": 1420961,
-            "partyid": 51054789
+            "partyid": 51054789,
+            "partyuuid": "5e95211a-f6c5-4991-bd47-890d2374b3b9"
         }
     },
     "org2": {
         "orgno": "313818608",
         "partyid": 51871883,
+        "partyuuid": "4236b831-41b0-49fa-aa52-02670b7acdf7",
         "dagl": {
             "pid": "02856099884",
             "lastname": "ALKOVE",
             "userid": 1278315,
-            "partyid": 51386822
+            "partyid": 51386822,
+            "partyuuid": "25651293-8520-4ea4-b89b-b6cf89b288fa"
         }
     },
     "org3": {
         "orgno": "312822881",
         "partyid": 51636963,
+        "partyuuid": "b2ca593f-c8bc-4c8d-8741-b7d59e0af859",
         "dagl": {
             "pid": "47827602783",
             "lastname": "ELEV",
             "userid": 1420963,
-            "partyid": 51275190
+            "partyid": 51275190,
+            "partyuuid": "2eca45b4-4e6d-4905-af22-82afc9b38f78"
         },
         "tilg": {
             "pid":"05859399586",
             "lastname": "DESIMETER",
             "userid": 1420959,
-            "partyid": 50553001
+            "partyid": 50553001,
+            "partyuuid": "52a806a2-3f97-4644-9fcf-6e82bab1478b"
         }
     },
     "randomUser": {
         "pid": "12896198376",
         "lastname": "KLOSETT",
         "userid": 1420960,
-        "partyid": 51032967
+        "partyid": 51032967,
+        "partyuuid": "1931d59f-3f7f-431d-9636-e609f9d06868"
 	},
     "org_system": {
         "orgno": "310422649",
         "partyid": 51562159,
-        "partyuuid": "4E9850BD-CDD4-4AB2-956D-44395D4F7EC1",
+        "partyuuid": "4e9850bd-cdd4-4ab2-956d-44395d4f7ec1",
         "systems": [
     	      "a3cecdb7-40b7-41a0-8ddd-e418b1abe83e"
         ],
@@ -67,7 +76,7 @@ module.exports =
             "lastname": "ALMANAKK",
             "userid": 1279051,
             "partyid": 50553336,
-            "partyuuid": "3ADDA6EF-2423-4493-BD6E-CA6DE858C2F1"
+            "partyuuid": "3adda6ef-2423-4493-bd6e-ca6de858c2f1"
         }
     }
 };
