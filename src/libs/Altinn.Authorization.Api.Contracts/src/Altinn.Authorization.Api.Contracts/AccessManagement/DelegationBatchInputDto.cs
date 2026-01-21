a1697c1d3c6f9556cc7a5669d0ce0499dab8ab7f
Andreas Isnes Nilsen <andreas.isnes@gmail.com>
2026-01-21 11:18:40 +0100

    fix contract (#2066)

--- src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/AccessManagement/DelegationBatchInputDto.cs
+++ src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/AccessManagement/DelegationBatchInputDto.cs
@@ -4,14 +4,15 @@ namespace Altinn.Authorization.Api.Contracts.AccessManagement;
 
 public class DelegationBatchInputDto
 {
+    [JsonPropertyName("values")]
     public List<Permission> Values { get; set; } = [];
 
     public class Permission
     {
         [JsonPropertyName("role")]
         public string Role { get; set; }
-        
-        [JsonPropertyName("package")]
+
+        [JsonPropertyName("packages")]
         public List<string> Packages { get; set; }
     }
 }
