d27a79cbf3f32f7e43694f47ff11803004c88e5f
Sneha Sirure <sneha.sirure@accenture.com>
2026-02-02 14:52:02 +0100

    Added bruno tests for klientdelegation API endpoints

--- src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/klient-delegation/at22.js
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/klient-delegation/at22.js
@@ -1,67 +1,77 @@
-module.exports = {
+﻿module.exports = {
   env: "at22",
   resources: {
     clientDelgResourceId: "devtest_gar_bruno_client_resource",
-  },
-  REGN_Organisasjon: {
-    name: "MOTSTANDSDYKTIG VASSEN TIGER AS",
-    orgno: "314243994",
-    partyId: 51237018,
-    partyUuid: "c18399ec-4c3f-4de2-991c-5c3cb6ccf50c",
-    dagligleder: {
-      name: "FORSTÅELSESFULL BYGD",
-      pid: "08885399984",
-      userId: 20752696,
-      partyId: 51147134,
-      partyUuid: "9c9938a9-81d9-41e8-92c1-9640c44f02d6",
-    },
-    REGN_Org_Clients: {
-      client_A: {
-        name: "AKADEMISK RU TIGER AS",
-        orgno: "213368532",
-        partyId: 51276113,
-        partyUuid: "c258d723-5925-40bc-95f3-b6abe37da21a",
-        dagligleder: {
-          name: "HALV BEHANDLING",
-          pid: "02826899428",
-          userId: 20266646,
-          partyId: 50928836,
-          partyUuid: "35938802-36b8-4c3d-9936-519e6a5582df",
+  },    
+    REGN_Organisasjon: {
+      name: "MOTSTANDSDYKTIG VASSEN TIGER AS",
+      orgno: "314243994",
+      partyId: 51237018,
+      partyUuid: "c18399ec-4c3f-4de2-991c-5c3cb6ccf50c",
+      dagligleder: {
+        name: "FORSTÅELSESFULL BYGD",
+        pid: "08885399984",
+        userId: 20752696,
+        partyId: 51147134,
+        partyUuid: "9c9938a9-81d9-41e8-92c1-9640c44f02d6",
+      },
+      REGN_Org_Clients: {
+        client_A: {
+          name: "AKADEMISK RU TIGER AS",
+          orgno: "213368532",
+          partyId: 51276113,
+          partyUuid: "c258d723-5925-40bc-95f3-b6abe37da21a",
+          dagligleder: {
+            name: "HALV BEHANDLING",
+            pid: "02826899428",
+            userId: 20266646,
+            partyId: 50928836,
+            partyUuid: "35938802-36b8-4c3d-9936-519e6a5582df",
+          },
+        },
+        client_B: {
+          name: "ALFABETISK INITIATIVRIK TIGER AS",
+          orgno: "313360210",
+          partyId: 51626870,
+          partyUuid: "b37fd4fc-829c-4601-8dc3-d7849d0f7764",
+          dagligleder: {
+            name: "DJERV ABONNEMENT",
+            pid: "20824699322",
+            userId: 20736127,
+            partyId: 50961667,
+            partyUuid: "991ba9d5-2507-4d92-8e01-2dd9534f37f8",
+          },
         },
       },
-      client_B: {
-        name: "ALFABETISK INITIATIVRIK TIGER AS",
-        orgno: "313360210",
-        partyId: 51626870,
-        partyUuid: "b37fd4fc-829c-4601-8dc3-d7849d0f7764",
-        dagligleder: {
-          name: "DJERV ABONNEMENT",
-          pid: "20824699322",
-          userId: 20736127,
-          partyId: 50961667,
-          partyUuid: "991ba9d5-2507-4d92-8e01-2dd9534f37f8",
-        },
+      REGN_Agent: {
+        etternavn: "GRANITT",
+        personidentity: "08919574934",
+        userId: 20600687,
+        partyId: 50485120,
+        userUuid: "7c6617f7-71d2-4235-99bf-c35658ae5161",
       },
     },
-    REGN_Agent: {
-      etternavn: "GRANITT",
-      personidentity: "08919574934",
-      userId: 20600687,
-      partyId: 50485120,
-      partyUuid: "7c6617f7-71d2-4235-99bf-c35658ae5161",
+    REVI_Organisasjon: {
+      name: "OVERFLADISK LANG TIGER AS",
+      orgno: "310267511",
+      partyId: 51235849,
+      partyUuid: "037a33d1-af1c-4156-9506-86d63a09f6a6",
+      dagligleder: {
+        name: "ROBUST SERVIETT",
+        pid: "25848395245",
+        userId: 20013182,
+        partyId: 50279107,
+        partyUuid: "37d0ffeb-96be-4434-ba7f-b9a4f3428e4f",
+      },
     },
-  },
-  REVI_Organisasjon: {
-    name: "OVERFLADISK LANG TIGER AS",
-    orgno: "310267511",
-    partyId: 51235849,
-    partyUuid: "037a33d1-af1c-4156-9506-86d63a09f6a6",
-    dagligleder: {
-      name: "ROBUST SERVIETT",
-      pid: "25848395245",
-      userId: 20013182,
-      partyId: 50279107,
-      partyUuid: "37d0ffeb-96be-4434-ba7f-b9a4f3428e4f",
+    innbygger_package_to_delegate: {
+      package_urn: "urn:altinn:accesspackage:innbygger-samliv",
     },
-  },
-};
+    privatPerson: {
+      name: "TRIST PAPPA",
+      pid: "18834599313",
+      userId: 20596375,
+      partyId: 51177430,
+      partyUuid: "7b7bc643-821f-4cb2-b874-9dc6eb58f5c5",
+    },
+  };
