e3dae6e29b00f55a0d4055710908dd367bad2eb2
Henning Normann <h.normann@accenture.com>
2026-02-08 23:47:44 +0100

    Temp disabling of new code to see effect on unit tests

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/ServiceCollectionExtensions.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Extensions/ServiceCollectionExtensions.cs
@@ -52,31 +52,31 @@ public static class ServiceCollectionExtensions
     private static void ConfigureNpgsql(NpgsqlDbContextOptionsBuilder builder)
     {
         builder.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
-        builder.ConfigureDataSource((dataSourceBuilder) =>
-        {
-            dataSourceBuilder.ConfigureTracing(o =>
-            {
-                o.EnableFirstResponseEvent(false);
-                o.ConfigureCommandEnrichmentCallback((activity, command) =>
-                {
-                    // Remove useless tags
-                    activity.SetTag("db.connection_id", null);
-                    activity.SetTag("db.connection_string", null);
-                    activity.SetTag("db.name", null);
-                    activity.SetTag("db.user", null);
-                    activity.SetTag("net.peer.ip", null);
-                    activity.SetTag("net.peer.name", null);
-                    activity.SetTag("net.transport", null);
+        ////builder.ConfigureDataSource((dataSourceBuilder) =>
+        ////{
+        ////    dataSourceBuilder.ConfigureTracing(o =>
+        ////    {
+        ////        o.EnableFirstResponseEvent(false);
+        ////        o.ConfigureCommandEnrichmentCallback((activity, command) =>
+        ////        {
+        ////            // Remove useless tags
+        ////            activity.SetTag("db.connection_id", null);
+        ////            activity.SetTag("db.connection_string", null);
+        ////            activity.SetTag("db.name", null);
+        ////            activity.SetTag("db.user", null);
+        ////            activity.SetTag("net.peer.ip", null);
+        ////            activity.SetTag("net.peer.name", null);
+        ////            activity.SetTag("net.transport", null);
 
-                    // Change statement tag to hash large queries and log the full query once per application lifetime
-                    activity.SetTag("db.statement", GetCommandTextHash(command.CommandText));
-                    if (command.Parameters.Count > 0)
-                    {
-                        activity.AddTag("db.command.parameters", GetParametersForLogging(command));
-                    }
-                });
-            });
-        });
+        ////            // Change statement tag to hash large queries and log the full query once per application lifetime
+        ////            activity.SetTag("db.statement", GetCommandTextHash(command.CommandText));
+        ////            if (command.Parameters.Count > 0)
+        ////            {
+        ////                activity.AddTag("db.command.parameters", GetParametersForLogging(command));
+        ////            }
+        ////        });
+        ////    });
+        ////});
     }
 
     private static string GetCommandTextHash(string commandText)
