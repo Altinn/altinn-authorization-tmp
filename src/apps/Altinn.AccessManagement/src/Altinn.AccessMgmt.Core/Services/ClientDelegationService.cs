dcd0c4e6aef90e1aa570e2d48b8e30e78942a5c3
Andreas Isnes Nilsen <andreas.isnes@gmail.com>
2026-02-10 14:30:43 +0100

    chores: add agent added at field to agent dto (#2209)
    * add modified at toagent
    
    * specifies when agent relationship was added
    
    * update docs for agentdto

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/ClientDelegationService.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/ClientDelegationService.cs
@@ -26,6 +26,7 @@ public class ClientDelegationService(AppDbContext db) : IClientDelegationService
         return query.Select(a => new AgentDto()
         {
             Agent = DtoMapper.Convert(a.To),
+            AgentAddedAt = a.Audit_ValidFrom,
             Access = [
                 new()
                 {
@@ -217,7 +218,7 @@ public class ClientDelegationService(AppDbContext db) : IClientDelegationService
             {
                 x.Delegation.To.To,
                 x.Delegation.From.Role,
-                x.DelegationPackage.Package
+                x.DelegationPackage.Package,
             })
             .GroupBy(x => x.To.Id)
             .ToListAsync(cancellationToken);
@@ -227,6 +228,7 @@ public class ClientDelegationService(AppDbContext db) : IClientDelegationService
             new AgentDto()
             {
                 Agent = DtoMapper.Convert(e.First().To),
+                AgentAddedAt = e.First().To.Audit_ValidFrom,
                 Access = e.GroupBy(r => r.Role.Id).Select(r => new AgentDto.AgentRoleAccessPackages
                 {
                     Role = DtoMapper.ConvertCompactRole(r.First().Role),
