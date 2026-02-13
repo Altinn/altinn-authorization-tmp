ab3a3632663f8215d35df0dac49d10e469c64f8a
Løvoll, Remi <acn-rlovo@ai-dev.no>
2026-02-13 15:28:51 +0100

    Added testdata

--- src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/resource-delegationcheck/tt02.js
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/resource-delegationcheck/tt02.js
@@ -1,6 +1,6 @@
 ﻿module.exports = 
 {
-  "env": "at22",
+  "env": "tt02",
   "resources": {
     "packageResourceId": "tilgangspakke_delegering_ressurs",
     "roleResourceId": "jks-test-resource"
@@ -8,71 +8,71 @@
   "VOKSENDE_FRYKTLØS_TIGER": {
     "name": "VOKSENDE FRYKTLØS TIGER AS",
     "orgno": "310842346",
-    "partyId": 51385701,
-    "partyUuid": "b005b932-cff1-464c-8cc4-8f2c15737857",
+    "partyId": 51599233,
+    "partyUuid": "5cee3261-b855-4d37-8673-5805b85e4609",
     "dagligleder": {
       "name": "LILLA TRIKK",
       "pid": "23836696743",
-      "userId": 20239658,
-      "partyId": 50276537,
-      "partyUuid": "2fd8a7fe-3217-4be4-8cfd-ef758cbda5e6"
+      "userId": 2303197,
+      "partyId": 50764392,
+      "partyUuid": "bf90c9d6-be6e-4071-84ff-bc18581e4a18"
     },
     "underenhet": {
       "name": "VOKSENDE FRYKTLØS TIGER AS",
       "orgno": "315690463",
-      "partyId": 51806608,
-      "partyUuid": "d32f820e-2e9c-4032-a92f-fb950da99923"
+      "partyId": 52019963,
+      "partyUuid": "0733915d-ca9a-440b-ad00-7d2a22ca2554"
     },
     "regnskapsfører": {
       "name": "HISTORISK AUTONOM TIGER AS",
       "orgno": "310349631",
-      "partyId": 51237150,
-      "partyUuid": "0852df5f-846c-4ec4-b665-9be659477f79",
+      "partyId": 51450696,
+      "partyUuid": "c7b44911-7b86-44a8-ae6e-5a9cc044375a",
       "dagligleder": {
         "name": "KONTROLLERT BOM",
         "pid": "08897397596",
-        "userId": 20709787,
-        "partyId": 50353779,
-        "partyUuid": "9385f8bf-ea6e-4c30-a493-b3c35f5b7d40"
+        "userId": 1426042,
+        "partyId": 50810484,
+        "partyUuid": "45c08ccd-ff46-4c1e-9d68-aa49aa771151"
       }
     },
     "revisor": {
       "name": "DIREKTE TØRR TIGER AS",
       "orgno": "314240588",
-      "partyId": 51236339,
-      "partyUuid": "dd425e80-c0bf-4695-b1f6-9c816c534c02",
+      "partyId": 51449867,
+      "partyUuid": "ddff9326-a607-4555-acee-085bbb232a62",
       "dagligleder": {
         "name": "PRESIS ERT",
         "pid": "14866498599",
-        "userId": 21104566,
-        "partyId": 50249863,
-        "partyUuid": "e703b470-3775-4320-b727-73d9f0cf802a"
+        "userId": 1271208,
+        "partyId": 50604244,
+        "partyUuid": "3bff3c81-0259-42f7-addf-83ddf107057d"
       }
     },
     "tilgangstyrer_med_enkeltrettighet": {
       "pid": "15927199618",
       "name": "TILGIVENDE PERIODE",
-      "userId": 21167067,
-      "partyId": 50942237,
-      "partyUuid": "f44a36e8-62ab-419c-bd0b-f8098ddd7e5d"
+      "userId": 2087214,
+      "partyId": 51193529,
+      "partyUuid": "8ea6e533-6b54-411f-a6f6-870b7081998b"
     },
     "tilgangstyrer_entity_med_enkeltrettighet": {
       "name": "RAKRYGGET BLOMSTRETE APE",
       "orgno": "311025503",
-      "partyId": 51401870,
-      "partyUuid": "a4042d10-2236-4c0b-a0e8-31cf2c221a32",
+      "partyId": 51615400,
+      "partyUuid": "10981e12-f827-48ec-a9d6-d98c15dad474",
       "dagligleder": {
         "name": "DIVERSE KINO",
         "pid": "03897298030",
-        "userId": 20144677,
-        "partyId": 50682674,
-        "partyUuid": "1bcfd2f4-bb94-490b-9fbc-4b4d60ef256f"
+        "userId": 1432206,
+        "partyId": 50718874,
+        "partyUuid": "1fa1aa67-fdf9-467f-b35a-02b466641100"
       },
       "underenhet": {
         "name": "RAKRYGGET BLOMSTRETE APE",
         "orgno": "314482263",
-        "partyId": 51696752,
-        "partyUuid": "4063f244-e480-49a5-b0c3-9c330760da1d"
+        "partyId": 51910125,
+        "partyUuid": "36052539-7a2f-452f-acb7-9ddd1826b946"
       }
     }
   }
