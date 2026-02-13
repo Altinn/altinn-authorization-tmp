c01957c2c6ad518e1d28e43c0027dec5a026d034
jonkjetiloye <40996230+jonkjetiloye@users.noreply.github.com>
2026-02-13 14:22:59 +0100

    bug: missing unique constraint on ResourceId (#2255)
    * bug: missing unique constraint on ResourceId
    #2254
    
    * Added EF migrations
    
    * Added missing codes
    
    ---------
    
    Co-authored-by: Jon Kjetil Øye <acn-joye@ai-dev.no>
    Co-authored-by: Marius Thuen <thuen@spirh.no>

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20260213101047_ResourceRolePackageConstraints.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.AccessMgmt.PersistenceEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResourceRolePackageConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_role_providerid_code",
+                schema: "dbo",
+                table: "role");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "refid",
+                schema: "dbo",
+                table: "resource",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "urn",
+                schema: "dbo",
+                table: "package",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "code",
+                schema: "dbo",
+                table: "package",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_role_code",
+                schema: "dbo",
+                table: "role",
+                column: "code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_resource_refid",
+                schema: "dbo",
+                table: "resource",
+                column: "refid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_package_code",
+                schema: "dbo",
+                table: "package",
+                column: "code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_package_urn",
+                schema: "dbo",
+                table: "package",
+                column: "urn",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_role_code",
+                schema: "dbo",
+                table: "role");
+
+            migrationBuilder.DropIndex(
+                name: "ix_resource_refid",
+                schema: "dbo",
+                table: "resource");
+
+            migrationBuilder.DropIndex(
+                name: "ix_package_code",
+                schema: "dbo",
+                table: "package");
+
+            migrationBuilder.DropIndex(
+                name: "ix_package_urn",
+                schema: "dbo",
+                table: "package");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "refid",
+                schema: "dbo",
+                table: "resource",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "urn",
+                schema: "dbo",
+                table: "package",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "code",
+                schema: "dbo",
+                table: "package",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_role_providerid_code",
+                schema: "dbo",
+                table: "role",
+                columns: new[] { "providerid", "code" },
+                unique: true);
+        }
+    }
+}
