c076223f82bb5bd7ff27e969a94d16854f9013c4
Marius Thuen <thuen@spirh.no>
2026-02-20 00:54:50 +0100

    Added AuditVersion Annotation v1

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20260219235346_AuditVersion_v1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.AccessMgmt.PersistenceEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditVersion_v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
