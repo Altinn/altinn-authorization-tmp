b2da218a206443914387fac8905151f01f2b2217
Løvoll, Remi <acn-rlovo@ai-dev.no>
2026-02-23 09:42:23 +0100

    Fixed copilot comments

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/Contracts/IRightImportProgressService.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/Contracts/IRightImportProgressService.cs
@@ -1,10 +1,4 @@
 ﻿using Altinn.AccessMgmt.PersistenceEF.Extensions;
-using Altinn.AccessMgmt.PersistenceEF.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Altinn.AccessMgmt.Core.Services.Contracts
 {
@@ -14,7 +8,7 @@ namespace Altinn.AccessMgmt.Core.Services.Contracts
         /// Check if a given delegation change has already been processed
         /// </summary>
         /// <param name="delegationChangeId">The identificator unique for the delegation change inside the same origin</param>
-        /// <param name="originType">the origin (ResourceRegistry, App or Instanse</param>
+        /// <param name="originType">the origin (ResourceRegistry, App or Instance</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
         /// <returns></returns>
         Task<bool> IsImportAlreadyProcessed(long delegationChangeId, string originType, CancellationToken cancellationToken);
@@ -23,7 +17,7 @@ namespace Altinn.AccessMgmt.Core.Services.Contracts
         /// Mark a given delegation change as already processed
         /// </summary>
         /// <param name="delegationChangeId">The identificator unique for the delegation change inside the same origin</param>
-        /// <param name="originType">the origin (ResourceRegistry, App or Instanse</param>
+        /// <param name="originType">the origin (ResourceRegistry, App or Instance</param>
         /// <param name="audit">required to call Savechanges not used</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
         /// <returns></returns>
