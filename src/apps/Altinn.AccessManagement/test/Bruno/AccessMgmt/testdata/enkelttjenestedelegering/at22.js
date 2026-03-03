eb5b538ec55cac7288444022ebbac196be68f242
Sneha Sirure <sneha.sirure@accenture.com>
2026-03-03 19:09:58 +0100

    Added tests for enkelttjeneste-delegering

---
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/enkelttjenestedelegering/at22.js
@@ -0,0 +1,58 @@
+﻿module.exports = 
+{
+  "env": "at22",
+  "resource_to_delegate": {
+    "resource_id_privatperson": "ttd-test-bruno-enkelttjeneste-privatperson-ressurs",
+    "resource_id_virksomhet": "ttd-test-bruno-enkelttjenestedelegering-virksomhet",
+    "app_id" : "app_ttd_test-bruno-app-for-delegation"
+
+  },
+  Bruker_A_delegates :
+  {
+    "name": "FLOTT ENERGI",
+    "pid": "19911999652",
+    "partyid": 50164373,
+    "userid": 20055011,
+    "partyuuid": "08cb10ee-4f3d-461a-8b11-a6604fdf2cc2"
+  }, 
+  Bruker_B_delegates : 
+  {
+    "name": "STILLE FELTSENG",
+    "pid": "12857399321",
+    "partyid": 50134437,
+    "userid": 21142643,
+    "partyuuid": "ef15253b-92a9-4045-9609-9323e39e6018"
+  }, 
+  orgA_delegates : 
+  {
+    "name": "SLEM JUSTERBAR KATT PALMEOLJE",
+    "org_no": "314318579",
+    "partyid": 51443260,
+    "partyuuid": "1785642b-52f6-4aa7-a8d5-6fc4e2991aa5",
+    "innehaver": {
+      "name": "OPPBLÅST HYBELKANIN",
+      "pid": "01875698776",
+      "partyid": 50208490,
+      "userid": 20263004,
+      "partyuuid": "34ca5d6a-e5e9-4df0-bbf6-28ee37cad437"
+    },
+  },
+  orgB_delegates : 
+  {
+    "name": "INTUITIV FUNKSJONELL ØGLE KONKURSBO",
+    "org_no": "313067750",
+    "partyid": 51514735,
+    "partyuuid": "7ca54822-ca92-4de2-a917-956f8f0afda6",
+    "bostyrer": {
+      "name": "FLEKSIBEL RIDEBUKSE",
+      "pid": "06816598638",
+      "partyid": 50231931,
+      "userid": 20072982,
+      "partyuuid": "0c99c618-dd73-48d6-8438-738ef869fdd2"
+    },
+  },
+
+
+
+
+}
---
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/enkelttjenestedelegering/tt02.js
@@ -0,0 +1,58 @@
+﻿module.exports = 
+{
+  "env": "at22",
+  "resource_to_delegate": {
+    "resource_id_privatperson": "ttd-test-bruno-enkelttjeneste-privatperson-ressurs",
+    "resource_id_virksomhet": "ttd-test-bruno-enkelttjenestedelegering-virksomhet",
+    "app_id" : "app_ttd_test-bruno-app-for-delegation"
+
+  },
+  Bruker_A_delegates :
+  {
+    "name": "FLOTT ENERGI",
+    "pid": "19911999652",
+    "partyid": 51059891,
+    "userid": 1884447,
+    "partyuuid": "60f9046c-5b8b-4f77-9e35-05413762a4f1"
+  }, 
+  Bruker_B_delegates : 
+  {
+    "name": "STILLE FELTSENG",
+    "pid": "12857399321",
+    "partyid": 50630763,
+    "userid": 2234872,
+    "partyuuid": "b00dea15-b341-48a0-9b04-33a396f61f70"
+  }, 
+  orgA_delegates : 
+  {
+    "name": "SLEM JUSTERBAR KATT PALMEOLJE",
+    "org_no": "314318579",
+    "partyid": 51580095,
+    "partyuuid": "7d9e8bf3-0014-4381-bdf1-8bdd726b9d02",
+    "innehaver": {
+      "name": "OPPBLÅST HYBELKANIN",
+      "pid": "01875698776",
+      "partyid": 50571568,
+      "userid": 2310168,
+      "partyuuid": "c129e311-f45c-4f27-828b-574bdcccab5a"
+    },
+  },
+  orgB_delegates : 
+  {
+    "name": "INTUITIV FUNKSJONELL ØGLE KONKURSBO",
+    "org_no": "313067750",
+    "partyid": 51728254,
+    "partyuuid": "b2e4c3a8-5ce8-447f-a6f4-7441b1bcf5d5",
+    "bostyrer": {
+      "name": "FLEKSIBEL RIDEBUKSE",
+      "pid": "06816598638",
+      "partyid": 50834853,
+      "userid": 1740333,
+      "partyuuid": "404e282c-918c-46cc-aa6a-46fd018768da"
+    },
+  },
+
+
+
+
+}
