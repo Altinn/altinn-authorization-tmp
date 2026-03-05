238527a500a459bd62c47d107b1a2d8882cd2405
jonkjetiloye <40996230+jonkjetiloye@users.noreply.github.com>
2026-03-05 19:45:54 +0100

    Bug: clear a2 cache on add rightholder (#2478)
    * bug: clear A2 cache on add new rightholder (in order to trigger new reportee to become available)
    
    * - remove add on resource. Add/Remove rightholder should be sufficient
    
    ---------
    
    Co-authored-by: Jon Kjetil Øye <acn-joye@ai-dev.no>

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/ConnectionService.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Services/ConnectionService.cs
@@ -128,6 +128,11 @@ public partial class ConnectionService(
         await dbContext.Assignments.AddAsync(assignment, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
 
+        if (from.PartyId.HasValue && to.PartyId.HasValue)
+        {
+            await altinn2Client.ClearReporteeRights(from.PartyId.Value, to.PartyId.Value, to.UserId.HasValue ? to.UserId.Value : 0, cancellationToken: cancellationToken);
+        }
+
         return DtoMapper.Convert(assignment);
     }
 
