6e3b8275624777812d37384e90eab7fea8dc4346
Jon Kjetil Øye <acn-joye@ai-dev.no>
2026-03-09 14:30:34 +0100

    feat: connections api | added instance endpoints
    - Controller endpoints and models for instance delegation
    
    #2489
    #2490
    #2491
    #2492
    #2493
    #2494
    #2495
    #2503

---
+++ src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/AccessManagement/InstancePermissionDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Altinn.Authorization.Api.Contracts.AccessManagement;
+
+/// <summary>
+/// Instance permissions
+/// </summary>
+public class InstancePermissionDto
+{
+    /// <summary>
+    /// Resource the permissions are for
+    /// </summary>
+    public ResourceDto Resource { get; set; }
+
+    /// <summary>
+    /// Instance the permissions are for
+    /// </summary>
+    public InstanceDto Instance { get; set; }
+
+    /// <summary>
+    /// Parties with permissions
+    /// </summary>
+    public IEnumerable<PermissionDto> Permissions { get; set; } 
+}
