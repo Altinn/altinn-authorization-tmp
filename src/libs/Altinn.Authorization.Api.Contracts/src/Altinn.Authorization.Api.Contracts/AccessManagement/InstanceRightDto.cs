6370adeac01f916182194546f5e3ae88aca789b0
jonkjetiloye <40996230+jonkjetiloye@users.noreply.github.com>
2026-03-10 15:08:23 +0100

    feat: connections api | added instance endpoints (#2505)
    - Controller endpoints and models for instance delegation
    
    #2489
    #2490
    #2491
    #2492
    #2493
    #2494
    #2495
    #2503
    
    Co-authored-by: Jon Kjetil Øye <acn-joye@ai-dev.no>

---
+++ src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/AccessManagement/InstanceRightDto.cs
@@ -0,0 +1,48 @@
+﻿namespace Altinn.Authorization.Api.Contracts.AccessManagement;
+
+/// <summary>
+/// Instance rights
+/// </summary>
+public class InstanceRightDto
+{
+    /// <summary>
+    /// Resource
+    /// </summary>
+    public ResourceDto Resource { get; set; }
+
+    /// <summary>
+    /// Instance
+    /// </summary>
+    public InstanceDto Instance { get; set; }
+
+    /// <summary>
+    /// Rights
+    /// </summary>
+    public List<RightPermission> Rights { get; set; }
+}
+
+/// <summary>
+/// External Dto for instance rights
+/// </summary>
+public class ExtInstanceRightDto
+{
+    /// <summary>
+    /// Resource
+    /// </summary>
+    public ResourceDto Resource { get; set; }
+
+    /// <summary>
+    /// Instance
+    /// </summary>
+    public InstanceDto Instance { get; set; }
+
+    /// <summary>
+    /// Direct Rights
+    /// </summary>
+    public List<RightPermission> DirectRights { get; set; }
+
+    /// <summary>
+    /// Indirect Rights (access through key-role parties or through main unit)
+    /// </summary>
+    public List<RightPermission> IndirectRights { get; set; }
+}
---
+++ src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/AccessManagement/InstanceTypeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Authorization.Api.Contracts.AccessManagement;
+
+/// <summary>
+/// Define the types of Instances
+/// </summary>
+public class InstanceTypeDto
+{
+    /// <summary>
+    /// Identity
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Name
+    /// </summary>
+    public string Name { get; set; }
+}
