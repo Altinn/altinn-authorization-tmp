cbd6132a6512fc0e08fdba1f55135695860cb910
Andreas Isnes <andreas.isnes@gmail.com>
2026-03-11 10:07:12 +0100

    disable jobs

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.Core/Outbox/ResourceRequestNotification.cs
@@ -0,0 +1,40 @@
+using System.Text.Json;
+using Altinn.AccessMgmt.PersistenceEF.Extensions;
+using Altinn.AccessMgmt.PersistenceEF.Models;
+using Altinn.Authorization.Integration.Platform.Notification;
+
+namespace Altinn.AccessMgmt.Core.Outbox;
+
+public class ResourceRequestNotification(IAltinnNotification notification) : IOutboxHandler
+{
+    public Task Handle(OutboxMessage message, CancellationToken cancellationToken)
+    {
+        var content = JsonSerializer.Deserialize<List<ResourceRequestNotificationMessage>>(message.Data);
+        notification.Send(new()
+        {
+            IdempotencyId = "",
+            SendersReference = "", 
+            AssociationDialogporten = new()
+            {
+
+            },
+            Recipient = new()
+            {
+                RecipientPerson = new()
+                {
+                    
+                },
+            },
+            Reminders = [],
+        });
+    }
+}
+
+public class ResourceRequestNotificationMessage
+{
+    public Guid From { get; set; }
+
+    public Guid To { get; set; }
+
+    public string Resource { get; set; }
+}
