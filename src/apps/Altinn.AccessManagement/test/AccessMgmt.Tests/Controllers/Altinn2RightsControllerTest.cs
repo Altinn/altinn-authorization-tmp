f088e71f564588be0ac28052d8a7172729dfafd4
Rune T. Larsen <rune.tommeras-larsen@digdir.no>
2026-03-17 19:47:15 +0100

    Fixed unused namespace

--- src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/Altinn2RightsControllerTest.cs
+++ src/apps/Altinn.AccessManagement/test/AccessMgmt.Tests/Controllers/Altinn2RightsControllerTest.cs
@@ -3,7 +3,6 @@ using System.Net.Http.Headers;
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using AccessMgmt.Tests.Mocks;
 using Altinn.AccessManagement.Controllers;
 using Altinn.AccessManagement.Core.Clients.Interfaces;
 using Altinn.AccessManagement.Core.Constants;
