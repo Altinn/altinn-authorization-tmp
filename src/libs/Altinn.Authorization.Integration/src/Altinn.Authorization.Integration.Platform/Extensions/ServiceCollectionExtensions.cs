327963ea68605004bb990e8200307e7cdaf111cb
Andreas Isnes <andreas.isnes@gmail.com>
2026-03-17 13:21:49 +0100

    fix plural stuff

--- src/libs/Altinn.Authorization.Integration/src/Altinn.Authorization.Integration.Platform/Extensions/ServiceCollectionExtensions.cs
+++ src/libs/Altinn.Authorization.Integration/src/Altinn.Authorization.Integration.Platform/Extensions/ServiceCollectionExtensions.cs
@@ -55,7 +55,7 @@ public static partial class ServiceCollectionExtensions
         })
         .AddNotification(opts =>
         {
-            opts.Endpoint = appsettings.Notification.Endpoint;
+            opts.Endpoint = appsettings.Notifications.Endpoint;
         })
         .AddRegister(opts =>
         {
