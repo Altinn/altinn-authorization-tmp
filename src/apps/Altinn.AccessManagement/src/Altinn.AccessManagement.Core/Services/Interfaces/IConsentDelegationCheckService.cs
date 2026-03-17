1be02240c75e349dd7942864e349b9ba7c548ad5
Rune T. Larsen <rune.tommeras-larsen@digdir.no>
2026-03-17 20:06:20 +0100

    File fix

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Core/Services/Interfaces/IConsentDelegationCheckService.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Core/Services/Interfaces/IConsentDelegationCheckService.cs
@@ -1,21 +1,6 @@
-﻿namespace Altinn.AccessManagement.Core.Services.Interfaces;
+﻿using Altinn.AccessManagement.Core.Models;
 
-/// <summary>
-/// Result of a consent delegation check, containing the actions the user is authorized to delegate.
-/// </summary>
-public class ConsentDelegationCheckResult
-{
-    /// <summary>
-    /// Gets or sets a value indicating whether the delegation check was successful.
-    /// </summary>
-    public bool IsSuccess { get; set; }
-
-    /// <summary>
-    /// Gets or sets the list of action URNs (e.g. "urn:oasis:names:tc:xacml:1.0:action:action-id:read") 
-    /// that the authenticated user is authorized to delegate on behalf of the party.
-    /// </summary>
-    public IEnumerable<string> DelegatableActions { get; set; } = [];
-}
+namespace Altinn.AccessManagement.Core.Services.Interfaces;
 
 /// <summary>
 /// Service for performing delegation checks for consent scenarios.
