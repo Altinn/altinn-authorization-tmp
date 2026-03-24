83cb46c360c08d15a47fe812e5d241d43c9ff312
Andreas Isnes Nilsen <andreas.isnes@gmail.com>
2026-03-24 09:24:28 +0100

    chores: update handler message (#2638)

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement/AccessManagementHost.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement/AccessManagementHost.cs
@@ -102,7 +102,7 @@ internal static partial class AccessManagementHost
             options.AppConnectionString = connectionStrings.AppSource;
             options.MigrationConnectionString = connectionStrings.MigrationSource;
             options.Source = appsettings.RunInitOnly ? SourceType.Migration : SourceType.App;
-            options.AddOutboxHandler<RequestApprovedNotificationHandler>("request_accepted");
+            options.AddOutboxHandler<RequestApprovedNotificationHandler>("request_approved");
             options.AddOutboxHandler<RequestPendingNotificationHandler>("request_pending");
         });
 
