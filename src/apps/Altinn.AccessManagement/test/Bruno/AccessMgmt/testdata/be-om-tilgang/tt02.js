e03dd8673f437980eb35ec9d19f8fcb92c40e6c4
Sneha Sirure <sneha.sirure@accenture.com>
2026-03-27 00:53:32 +0100

    Added bruno tests for instans-delegering and DelegationRequest API endpoints

---
+++ src/apps/Altinn.AccessManagement/test/Bruno/AccessMgmt/testdata/be-om-tilgang/tt02.js
@@ -0,0 +1,77 @@
+﻿module.exports = {
+  env: "at22",
+  resource_to_delegate: {
+    resource_id_privatperson:
+      "ttd-test-bruno-enkelttjeneste-privatperson-ressurs",
+    resource_id_virksomhet:
+      "ttd-test-bruno-enkelttjenestedelegering-virksomhet",
+    app_id: "app_ttd_test-bruno-app-for-delegation",
+    skattetaten_ressurs: "ske-informasjon-om-trekkpaalegg",
+  },
+  Bot_person: {
+    lastname: "INGREDIENS",
+    pid: "21866699620",
+    partyid: 50911351,
+    userid: 2055277,
+    partyuuid: "877a433e-0c99-4b03-a75b-76ff364796d0",
+  },
+  Bot_from_person: {
+    lastname: "KARIES",
+    pid: "06920848050",
+    partyid: 51356827,
+    userid: 1918480,
+    partyuuid: "68a6555a-1a8b-4067-bd3f-1fccceeba161",
+  },
+  Bot_Org_for_serviceowner: {
+    name: "STERK DYP TIGER AS",
+    org_no: "313025098",
+    partyid: 51816948,
+    partyuuid: "722e5c50-8483-423a-a050-c7d7bcd88dfd",
+    dagligleder: {
+      name: "Materialistisk Bygg",
+      pid: "22816399142",
+      partyid: 51437759,
+      userid: 2264193,
+      partyuuid: "b6c4a1c5-dec6-4ebb-9629-04ca045dfaca",
+    },
+  },
+  Bot_person_serviceowner : {
+    lastname: "STAMMOR",
+    pid: "08826499630",
+    partyid: 51207503,
+    userid: 2417132,
+    partyuuid: "d970396b-0cf9-49e8-8ee1-e2f07aa74ec5",
+  },
+
+  Bot_Org: {
+    name: "PASSIV HANDLEKRAFTIG PUMA",
+    org_no: "313657892",
+    partyid: 51860826,
+    partyuuid: "fa3c7224-3d9f-44f8-ab7a-59f82fb18ea1",
+    innehaver: {
+      name: "FROM ANDAKT",
+      pid: "04907397659",
+      partyid: 50773665,
+      userid: 2439278,
+      partyuuid: "de72e5f0-7c3c-46bc-8c8c-4ab0d25ea0a0",
+    },
+  },
+
+  Bot_Org_2: {
+    name: "RIMELIG FAST HEST BORETTSLAG",
+    org_no: "310413089",
+    partyid: 51561311,
+    partyuuid: "1a93a7b3-e3de-416b-bc43-4a4e9640327e",
+    styreleder: {
+      name: "Lysegul Femkant",
+      pid: "14838799907",
+      partyid: 50971139,
+      userid: 1286384,
+      partyuuid: "02f2032f-60a0-49d0-ab29-8bfa6494cb35",
+    },
+  },
+  serviceowner_digdir: {
+    org: "digdir",
+    orgno: "991825827",
+  },
+};
