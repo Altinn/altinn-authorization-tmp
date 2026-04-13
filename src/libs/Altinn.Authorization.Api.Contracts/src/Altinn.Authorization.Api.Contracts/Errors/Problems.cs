cfb3ee9b1b047e7783a50fa4825c465f3c11b354
Rune T. Larsen <rune.tommeras-larsen@digdir.no>
2026-04-13 13:07:27 +0200

    merge

--- src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/Errors/Problems.cs
+++ src/libs/Altinn.Authorization.Api.Contracts/src/Altinn.Authorization.Api.Contracts/Errors/Problems.cs
@@ -146,16 +146,11 @@ public static class Problems
     public static ProblemDescriptor MissingRightKey { get; }
     = _factory.Create(33, HttpStatusCode.BadRequest, "No right key provided");
 
-
-
     /// <summary>Gets a <see cref="ProblemDescriptor"/>.</summary>
     public static ProblemDescriptor PackageDelegationNotAuthorized { get; }
     = _factory.Create(34, HttpStatusCode.Forbidden, "Service owner is not authorized to delegate this access package");
 
-
-    public static ProblemDescriptor MissingRightKey { get; }
-    = _factory.Create(33, HttpStatusCode.BadRequest, "No right key provided");
-
+    /// <summary>Gets a <see cref="ProblemDescriptor"/>.</summary>
     public static ProblemDescriptor PackageNotFound { get; }
     = _factory.Create(35, HttpStatusCode.BadRequest, "Unknown Access Package");
 }
