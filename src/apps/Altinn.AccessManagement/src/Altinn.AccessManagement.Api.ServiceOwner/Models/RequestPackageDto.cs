5ec27916133af6c4f0aeb5919faf3fff03715150
Marius Thuen <thuen@spirh.no>
2026-04-17 00:27:49 +0200

    Moved query params to body - new dto for creating requests

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.ServiceOwner/Models/RequestPackageDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Altinn.AccessManagement.Api.ServiceOwner.Models;
+
+/// <summary>
+/// Request a package
+/// </summary>
+public class RequestPackageDto
+{
+    /// <summary>
+    /// The request is from user wanting the package
+    /// Identify with valid urn see _meta/urns/party
+    /// </summary>
+    public string From { get; set; }
+
+    /// <summary>
+    /// The request is sent to the organization giving the package
+    /// Identify with valid urn see _meta/urns/party
+    /// </summary>
+    public string To { get; set; }
+
+    /// <summary>
+    /// The package identified with uuid or urn
+    /// </summary>
+    public string Package { get; set; }
+}
---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Api.ServiceOwner/Models/RequestResourceDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Altinn.AccessManagement.Api.ServiceOwner.Models;
+
+/// <summary>
+/// Request a resource
+/// </summary>
+public class RequestResourceDto
+{
+    /// <summary>
+    /// The request is from user wanting the package
+    /// Identify with valid urn see _meta/urns/party
+    /// </summary>
+    public string From { get; set; }
+
+    /// <summary>
+    /// The request is sent to the organization giving the package
+    /// Identify with valid urn see _meta/urns/party
+    /// </summary>
+    public string To { get; set; }
+
+    /// <summary>
+    /// The resource identified with resourceregistry resourceId
+    /// </summary>
+    public string Resource { get; set; }
+
+    /// <summary>
+    /// The specific keys for the resource
+    /// </summary>
+    public string[]? RightKeys { get; set; }
+}
