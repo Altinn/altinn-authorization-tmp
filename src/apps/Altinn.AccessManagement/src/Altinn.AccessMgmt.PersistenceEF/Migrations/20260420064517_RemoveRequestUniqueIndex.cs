b35ca0f941fe38738eeb0f62840da5889cce1af8
Marius Thuen <mariusthuen@gmail.com>
2026-04-20 12:35:53 +0200

    Removed unique constraint. Added Status to included. (#2875)

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/20260420064517_RemoveRequestUniqueIndex.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.AccessMgmt.PersistenceEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRequestUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_requestassignmentresource_assignmentid_resourceid_action_st~",
+                schema: "dbo",
+                table: "requestassignmentresource");
+
+            migrationBuilder.DropIndex(
+                name: "ix_requestassignmentpackage_assignmentid_packageid_status",
+                schema: "dbo",
+                table: "requestassignmentpackage");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_requestassignmentresource_assignmentid_resourceid",
+                schema: "dbo",
+                table: "requestassignmentresource",
+                columns: new[] { "assignmentid", "resourceid" })
+                .Annotation("Npgsql:IndexInclude", new[] { "status" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_requestassignmentpackage_assignmentid_packageid",
+                schema: "dbo",
+                table: "requestassignmentpackage",
+                columns: new[] { "assignmentid", "packageid" })
+                .Annotation("Npgsql:IndexInclude", new[] { "status" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_requestassignmentresource_assignmentid_resourceid",
+                schema: "dbo",
+                table: "requestassignmentresource");
+
+            migrationBuilder.DropIndex(
+                name: "ix_requestassignmentpackage_assignmentid_packageid",
+                schema: "dbo",
+                table: "requestassignmentpackage");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_requestassignmentresource_assignmentid_resourceid_action_st~",
+                schema: "dbo",
+                table: "requestassignmentresource",
+                columns: new[] { "assignmentid", "resourceid", "action", "status" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_requestassignmentpackage_assignmentid_packageid_status",
+                schema: "dbo",
+                table: "requestassignmentpackage",
+                columns: new[] { "assignmentid", "packageid", "status" },
+                unique: true);
+        }
+    }
+}
