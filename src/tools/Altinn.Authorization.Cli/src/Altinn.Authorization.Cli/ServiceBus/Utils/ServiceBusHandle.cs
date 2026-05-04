8c22c634cc34531efbeffa220aefb826d628f87c
Aleksander Heintz <alxandr@alxandr.me>
2026-05-04 14:06:26 +0200

    ﻿chore(cli): improve terminal running of the cli (#3003)

--- src/tools/Altinn.Authorization.Cli/src/Altinn.Authorization.Cli/ServiceBus/Utils/ServiceBusHandle.cs
+++ src/tools/Altinn.Authorization.Cli/src/Altinn.Authorization.Cli/ServiceBus/Utils/ServiceBusHandle.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using Azure.Identity;
+﻿using Altinn.Authorization.Cli.Utils;
 using Azure.Messaging.ServiceBus;
 using Azure.Messaging.ServiceBus.Administration;
 using CommunityToolkit.Diagnostics;
@@ -16,18 +15,7 @@ internal class ServiceBusHandle
             && uri.Scheme == "sb")
         {
             // Connection string is a endpoint URI
-            var options = new DefaultAzureCredentialOptions
-            {
-                ExcludeInteractiveBrowserCredential = false,
-            };
-
-            var query = uri.Query.Length > 1 ? HttpUtility.ParseQueryString(uri.Query.Substring(1)) : [];
-            if (query.Get("tenantId") is { Length: > 0 } tenantId)
-            {
-                options.TenantId = tenantId;
-            }
-
-            var credential = new DefaultAzureCredential(options);
+            var credential = AzureCredentialHelper.GetCredential(uri);
             var client = new ServiceBusClient(uri.Host, credential);
             var administrationClient = new ServiceBusAdministrationClient(uri.Host, credential);
 
