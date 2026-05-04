9771dd626f4086801c53a50b6d6d40c2e35b8048
Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>
2026-05-04 23:12:54 +0200

    remove pagesize and update add more tests

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessManagement.Core/Configuration/ConsentSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.AccessManagement.Core.Configuration
+{
+    public class ConsentSettings
+    {
+        public int LatestChangesPageSize { get; set; } = 1000;
+    }
+}
