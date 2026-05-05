2d5f9668e035ffdc9372f3e8acf758672dc61580
Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>
2026-05-05 09:11:51 +0200

    add pagesize customisation for consent list

--- src/apps/Altinn.AccessManagement/infra/main.tf
+++ src/apps/Altinn.AccessManagement/infra/main.tf
@@ -196,6 +196,7 @@ module "appsettings" {
       values = {
         "ConsentMigration:BatchSize"                  = { value = tostring(var.configuration.consent.batch_size) }
         "ConsentMigration:MaxDegreeOfParallelism"     = { value = tostring(var.configuration.consent.max_degree_of_parallelism) }
+        "Consent:LatestChangesPageSize"                 = { value = tostring(var.configuration.consent.latest_changes_page_size) }
         "Core:Request:NotifyRequestApprovedInSeconds" = { value = tostring(var.configuration.core.request_notify_request_approved_in_seconds) } # Deprecated
         "Core:Request:NotifyRequestPendingInSeconds"  = { value = tostring(var.configuration.core.request_notify_request_pending_in_seconds) }  # Deprecated
 
