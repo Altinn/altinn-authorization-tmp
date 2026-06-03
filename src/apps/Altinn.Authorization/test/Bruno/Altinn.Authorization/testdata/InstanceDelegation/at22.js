6c868dc74adeec64285e8fadab133d0763d8f064
Ivar Olav Tryti <aas-iotryti@ai-dev.no>
2026-06-03 12:47:08 +0200

    La til "ekte" testdata for PDP tester som feilet pga. fake testdata

--- src/apps/Altinn.Authorization/test/Bruno/Altinn.Authorization/testdata/InstanceDelegation/at22.js
+++ src/apps/Altinn.Authorization/test/Bruno/Altinn.Authorization/testdata/InstanceDelegation/at22.js
@@ -3,29 +3,30 @@ module.exports =
     "env": "at22",
     "resources": {
         "appInstanceDelegation": {
-            "resourceId": "app_ttd_authz-bruno-instancedelegation",
+            "resourceId": "app_ttd_ttd-bruno-tilgangspakke-app",
             "org": "ttd",
-            "app":  "authz-bruno-instancedelegation"
+            "app":  "ttd-bruno-tilgangspakke-app"
         }
     },
     "instances": {
         "p2p": {
-            "instanceId": "93994414-07dc-4df3-b2c6-047722169b6c",
+            "instanceId": "7ff72acc-1714-4991-a1d1-a7e89bd5e62d",
+            "brukerstyrtInstanceId": "7e6c9a38-13e2-4e5d-84ff-86a2903eb596",
             "from": "5daf2382-adee-4cd6-9995-3c5f3fc058af",
             "to": "bd90e954-efe5-4b65-be8a-f9fccbb36342"
         },
         "main2p": {
-            "instanceId": "df333e75-5896-4254-a69f-146736eaf668",
+            "instanceId": "6240aaf3-5795-4698-b1ac-1a18224eff22",
             "from": "01712d4f-cfb2-4d12-8aac-2eb7ad0d9c86",
             "to": "bd90e954-efe5-4b65-be8a-f9fccbb36342"
         },
         "sub2p": {
-            "instanceId": "49513daa-beb1-4a83-a07e-83da1378c8a9",
+            "instanceId": "238dd66a-8357-4da9-969b-5c53f3246f38",
             "from": "e21a640b-9558-4d59-b47f-7b87d2cd9ee0",
             "to": "bd90e954-efe5-4b65-be8a-f9fccbb36342"
         },
         "o2o": {
-            "instanceId": "0d55d70c-6e81-4728-b0e6-7ff1ede1a4fe",
+            "instanceId": "bd91ee77-cfd0-4797-8e3c-553bd8a14ee9",
             "from": "f7e2eb77-6b5e-4aeb-9fe9-f261013401c9",
             "to": "01712d4f-cfb2-4d12-8aac-2eb7ad0d9c86"
         }
