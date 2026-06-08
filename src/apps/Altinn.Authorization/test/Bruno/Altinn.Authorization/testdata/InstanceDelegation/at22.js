c033d1e43dbbf84a9895bcfb2cf5e5ad2fe62f9b
Ivar Olav Tryti <aas-iotryti@ai-dev.no>
2026-06-08 13:27:05 +0200

    la til 5 nye PDP tester (H2U, P2H, P2U, U2H, og U2U) og fjernet test som ikke brukes mer

--- src/apps/Altinn.Authorization/test/Bruno/Altinn.Authorization/testdata/InstanceDelegation/at22.js
+++ src/apps/Altinn.Authorization/test/Bruno/Altinn.Authorization/testdata/InstanceDelegation/at22.js
@@ -15,15 +15,32 @@ module.exports =
             "from": "5daf2382-adee-4cd6-9995-3c5f3fc058af",
             "to": "bd90e954-efe5-4b65-be8a-f9fccbb36342"
         },
+        "p2main": {
+            "instanceId": "d1f6410e-e5f1-406f-b0f6-8605f95bc867",
+            "from": "5daf2382-adee-4cd6-9995-3c5f3fc058af",
+            "to": "f7e2eb77-6b5e-4aeb-9fe9-f261013401c9"
+        },
+        "p2sub": {
+            "instanceId": "259dde98-8223-4e07-94c2-574a5a7c2a1c"
+        },
         "main2p": {
             "instanceId": "6240aaf3-5795-4698-b1ac-1a18224eff22",
             "from": "01712d4f-cfb2-4d12-8aac-2eb7ad0d9c86",
             "to": "bd90e954-efe5-4b65-be8a-f9fccbb36342"
         },
+        "main2sub": {
+            "instanceId": "f4777946-ecf6-43fc-b740-7197980e97c2",
+        },
         "sub2p": {
             "instanceId": "238dd66a-8357-4da9-969b-5c53f3246f38",
             "from": "e21a640b-9558-4d59-b47f-7b87d2cd9ee0",
             "to": "bd90e954-efe5-4b65-be8a-f9fccbb36342"
+        },
+        "sub2main": {
+            "instanceId": "5b8a4dd5-f281-4c78-a05a-b9e3a8e47f07",
+        },
+        "sub2sub": {
+            "instanceId": "2a4d49e1-4b54-4079-9b69-73e37d528299",
         },
         "o2o": {
             "instanceId": "bd91ee77-cfd0-4797-8e3c-553bd8a14ee9",
@@ -74,6 +91,50 @@ module.exports =
                 "userId": 20012882,
                 "partyUuid": "5daf2382-adee-4cd6-9995-3c5f3fc058af",
                 "partyId": 51195601
+            }
+        },
+        "ivrigGildTigerAS": {
+            "partyUuid": "87b0a6ff-d3e2-466e-a7d1-e52879425e43",
+            "name": "IVRIG GILD TIGER AS",
+            "organizationNumber": "311111043",
+            "partyId": 51409428,
+            "subunits": {
+                "ivrigGildTigerAS": {
+                    "partyUuid": "7d4602d6-5692-4f7b-b16a-91a380cdbc0c",
+                    "name": "LEGITIM JUSTERBAR TIGER AS",
+                    "organizationNumber": "314903331",
+                    "partyId": 51735042,
+                    "dagl": null
+                }
+            },
+            "dagl": {
+                "name": "SUPPORTER ARITMETISK",
+                "personId": "19886098742",
+                "userId": 20967956,
+                "partyUuid": "ca094035-f9cb-48b9-8f34-a37af4b66e96",
+                "partyId": 50972013
+            }
+        },
+        "slakkAktivBilleIKS": {
+            "partyUuid": "e5789232-f227-418d-a1c9-87a28fc25e7e",
+            "name": "SLAKK AKTIV BILLE IKS",
+            "organizationNumber": "314248333",
+            "partyId": 51269977,
+            "subunits": {
+                "slakkAktivBilleIKS": {
+                    "partyUuid": "3599450f-c15e-4a30-868f-0cf1f8f7ea29",
+                    "name": "SLAKK AKTIV BILLE IKS",
+                    "organizationNumber": "314612086",
+                    "partyId": 51708559,
+                    "dagl": null
+                }
+            },
+            "dagl": {
+                "name": "RØDFOTSULE AKSEPTABEL",
+                "personId": "14878498454",
+                "userId": 20763541,
+                "partyUuid": "9ee440a2-8cb9-43da-a2d8-962db5b53ab2",
+                "partyId": 50161361
             }
         }
     },
--- src/apps/Altinn.Authorization/test/Bruno/Altinn.Authorization/testdata/InstanceDelegation/tt02.js
+++ src/apps/Altinn.Authorization/test/Bruno/Altinn.Authorization/testdata/InstanceDelegation/tt02.js
@@ -15,15 +15,30 @@ module.exports =
             "from": "f9b2750f-f2aa-442c-91b7-418e2f627a8b",
             "to": "07336d43-1f0e-40a8-a02b-2c6019013cc7"
         },
+        "p2main": {
+            "instanceId": "f87c5de4-bc52-4f06-924e-3e88f7884e2d"
+        },
+        "p2sub": {
+            "instanceId": "6d5a2f37-91ed-4295-be17-ad30c8dba159"
+        },
         "main2p": {
             "instanceId": "49986993-2cec-4e96-85a5-b07c5b7e4c5e",
             "from": "58c0acd2-793e-48f1-a19f-169c171066f4",
             "to": "07336d43-1f0e-40a8-a02b-2c6019013cc7"
         },
+        "main2sub": {
+            "instanceId": "87e3d468-0b02-4135-bd9d-efdcde977062",
+        },
         "sub2p": {
             "instanceId": "3a875fb6-4610-4436-b179-df45bdbc9fa5",
             "from": "65018cec-9bd4-45ad-998d-357ac181eaf3",
             "to": "07336d43-1f0e-40a8-a02b-2c6019013cc7"
+        },
+        "sub2main": {
+            "instanceId": "a427957b-bb32-4321-89a5-d4d9f8f23bef",
+        },
+        "sub2sub": {
+            "instanceId": "28b94645-5e5c-4cb0-868c-8bbaad9d250c",
         },
         "o2o": {
             "instanceId": "632fe488-1ed3-4bff-ba9c-56f1df0839fc",
@@ -74,6 +89,50 @@ module.exports =
                 "userId": 1424054,
                 "partyUuid": "f9b2750f-f2aa-442c-91b7-418e2f627a8b",
                 "partyId": 51443447
+            }
+        },
+        "ivrigGildTigerAS": {
+            "partyUuid": "33388dcf-f785-40a5-80f8-536c01b459d6",
+            "name": "IVRIG GILD TIGER AS",
+            "organizationNumber": "311111043",
+            "partyId": 51622964,
+            "subunits": {
+                "ivrigGildTigerAS": {
+                    "partyUuid": "62f90166-9095-49e7-a476-bfb60b3600c9",
+                    "name": "LEGITIM JUSTERBAR TIGER AS",
+                    "organizationNumber": "314903331",
+                    "partyId": 51948403,
+                    "dagl": null
+                }
+            },
+            "dagl": {
+                "name": "SUPPORTER ARITMETISK",
+                "personId": "19886098742",
+                "userId": 2572394,
+                "partyUuid": "fc8a6b0d-9553-4aad-8c96-d4c5f5452d00",
+                "partyId": 51222916
+            }
+        },
+        "slakkAktivBilleIKS": {
+            "partyUuid": "16071f0d-18bb-4d49-bebc-167df5ebf3fd",
+            "name": "SLAKK AKTIV BILLE IKS",
+            "organizationNumber": "314248333",
+            "partyId": 51483492,
+            "subunits": {
+                "slakkAktivBilleIKS": {
+                    "partyUuid": "3edb1370-e03e-4ab7-af1b-f95369f9c5b7",
+                    "name": "SLAKK AKTIV BILLE IKS",
+                    "organizationNumber": "314612086",
+                    "partyId": 51921926,
+                    "dagl": null
+                }
+            },
+            "dagl": {
+                "name": "RØDFOTSULE AKSEPTABEL",
+                "personId": "14878498454",
+                "userId": 343084,
+                "partyUuid": "61db9171-b8c1-4500-a401-3479db09196f",
+                "partyId": 50833737
             }
         }
     },
