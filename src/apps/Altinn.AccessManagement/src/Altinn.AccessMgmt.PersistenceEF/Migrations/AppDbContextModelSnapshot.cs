eac621688021c529e025e16edc78abb651c52aff
Løvoll, Remi <acn-rlovo@ai-dev.no>
2026-06-11 14:49:52 +0200

    Also set the columd not nullable as Required

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/AppDbContextModelSnapshot.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Migrations/AppDbContextModelSnapshot.cs
@@ -3321,6 +3321,7 @@ namespace Altinn.AccessMgmt.PersistenceEF.Migrations
                     NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<long>("Id"));
 
                     b.Property<string>("ResourceIdentifier")
+                        .IsRequired()
                         .HasColumnType("text")
                         .HasColumnName("resourceidentifier");
 
