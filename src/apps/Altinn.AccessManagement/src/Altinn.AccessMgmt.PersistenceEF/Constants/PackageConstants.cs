220bd00542abc90e0b078c8f5d8af059c1d559d5
Ivar Olav Tryti <aas-iotryti@ai-dev.no>
2026-06-15 15:19:17 +0200

    Undoet urelatert endring

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Constants/PackageConstants.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Constants/PackageConstants.cs
@@ -1,6 +1,6 @@
-﻿using Altinn.AccessMgmt.PersistenceEF.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+using Altinn.AccessMgmt.PersistenceEF.Models;
 using Altinn.AccessMgmt.PersistenceEF.Utils;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Altinn.AccessMgmt.PersistenceEF.Constants;
 
