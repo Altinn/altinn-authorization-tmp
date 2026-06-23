b155e508f6798b7e4ebbc00978e32178fc10d990
Løvoll, Remi <acn-rlovo@ai-dev.no>
2026-06-23 12:38:01 +0200

    Table holding A2ClientDelegation info

---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Configurations/A2ClientRoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Altinn.AccessMgmt.PersistenceEF.Extensions;
+using Altinn.AccessMgmt.PersistenceEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Altinn.AccessMgmt.PersistenceEF.Configurations;
+
+public class A2ClientRoleConfiguration : IEntityTypeConfiguration<A2ClientRole>
+{
+    public void Configure(EntityTypeBuilder<A2ClientRole> builder)
+    {
+        builder.ToDefaultTable();
+
+        builder.HasKey(p => p.Id);
+    }
+}
---
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Models/Base/BaseA2ClientRole.cs
@@ -0,0 +1,37 @@
+﻿using Altinn.AccessMgmt.PersistenceEF.Models.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altinn.AccessMgmt.PersistenceEF.Models.Base
+{
+    public class BaseA2ClientRole
+    {
+        private Guid _id;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseA2ClientRole"/> class.
+        /// </summary>
+        public BaseA2ClientRole()
+        {
+            Id = Guid.CreateVersion7();
+        }
+
+        /// <summary>
+        /// Identity
+        /// </summary>
+        public Guid Id
+        {
+            get => _id;
+            set
+            {
+                if (!value.IsVersion7Uuid())
+                {
+                    throw new ArgumentException("Id must be a version 7 UUID", nameof(value));
+                }
+
+                _id = value;
+            }
+        }
+    }
+}
