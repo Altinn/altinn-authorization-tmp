9929d0c854616e0105b03aea49adf09b981047dc
Jon Kjetil Øye <acn-joye@ai-dev.no>
2026-07-24 08:02:39 +0200

    pr feedback fixes - made guid hashsets nullable as input

--- src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Queries/Connection/ConnectionQueryExtensions.cs
+++ src/apps/Altinn.AccessManagement/src/Altinn.AccessMgmt.PersistenceEF/Queries/Connection/ConnectionQueryExtensions.cs
@@ -7,7 +7,7 @@ namespace Altinn.AccessMgmt.PersistenceEF.Queries.Connection;
 /// </summary>
 internal static class ConnectionQueryExtensions
 {
-    internal static IQueryable<ConnectionQueryBaseRecord> ToIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid> ids)
+    internal static IQueryable<ConnectionQueryBaseRecord> ToIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid>? ids)
     {
         if (ids is null || ids.Count == 0)
         {
@@ -23,7 +23,7 @@ internal static class ConnectionQueryExtensions
         return query.Where(t => ids.Contains(t.ToId));
     }
 
-    internal static IQueryable<ConnectionQueryBaseRecord> FromIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid> ids, bool applyFromFilter = true)
+    internal static IQueryable<ConnectionQueryBaseRecord> FromIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid>? ids, bool applyFromFilter = true)
     {
         if (!applyFromFilter || ids is null || ids.Count == 0)
         {
@@ -39,7 +39,7 @@ internal static class ConnectionQueryExtensions
         return query.Where(t => ids.Contains(t.FromId));
     }
 
-    internal static IQueryable<ConnectionQueryBaseRecord> ViaIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid> ids)
+    internal static IQueryable<ConnectionQueryBaseRecord> ViaIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid>? ids)
     {
         if (ids is null || ids.Count == 0)
         {
@@ -55,7 +55,7 @@ internal static class ConnectionQueryExtensions
         return query.Where(t => t.ViaId.HasValue && ids.Contains(t.ViaId.Value));
     }
 
-    internal static IQueryable<ConnectionQueryBaseRecord> RoleIdExcludes(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid> ids)
+    internal static IQueryable<ConnectionQueryBaseRecord> RoleIdExcludes(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid>? ids)
     {
         if (ids is null || ids.Count == 0)
         {
@@ -71,7 +71,7 @@ internal static class ConnectionQueryExtensions
         return query.Where(t => !ids.Contains(t.RoleId));
     }
 
-    internal static IQueryable<ConnectionQueryBaseRecord> RoleIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid> ids)
+    internal static IQueryable<ConnectionQueryBaseRecord> RoleIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid>? ids)
     {
         if (ids is null || ids.Count == 0)
         {
@@ -87,7 +87,7 @@ internal static class ConnectionQueryExtensions
         return query.Where(t => ids.Contains(t.RoleId));
     }
 
-    internal static IQueryable<ConnectionQueryBaseRecord> ViaRoleIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid> ids)
+    internal static IQueryable<ConnectionQueryBaseRecord> ViaRoleIdContains(this IQueryable<ConnectionQueryBaseRecord> query, HashSet<Guid>? ids)
     {
         if (ids is null || ids.Count == 0)
         {
